c8cfbbf466f850647fce6fb738cd629a9f240aec
Frank LaNasa <fjlanasa@Franks-MacBook-Pro.local>
2020-03-06 16:22:39 -0500

    add filters min_date and max_date

--- lib/arrow_web/controllers/disruption_api_controller.ex
+++ lib/arrow_web/controllers/disruption_api_controller.ex
@@ -1,11 +1,51 @@
 defmodule ArrowWeb.DisruptionApiController do
   use ArrowWeb, :controller
   alias Arrow.{Repo, Disruption}
+  import Ecto.Query
+
+  @filters ~w{min_start_date max_start_date min_end_date max_end_date}
 
   def index(conn, params) do
+    query = params |> take_filters |> format_filters |> build_query
+
     render(conn, "index.json-api",
-      data: Repo.all(Disruption),
-      opts: [include: Map.get(params, "include"), fields: Map.get(params, "fields")]
+      data: Repo.all(query),
+      opts: [include: Map.get(params, "include")]
     )
   end
+
+  defp build_query(filters) do
+    Enum.reduce(filters, from(d in Disruption), &compose_query/2)
+  end
+
+  defp compose_query({filter, date}, query) when filter in ["min_start_date", "min_end_date"] do
+    filter_field = String.to_atom(String.slice(filter, 4..-1))
+    where(query, [d], field(d, ^filter_field) > ^date)
+  end
+
+  defp compose_query({filter, date}, query) when filter in ["max_start_date", "max_end_date"] do
+    filter_field = String.to_atom(String.slice(filter, 4..-1))
+    where(query, [d], field(d, ^filter_field) < ^date)
+  end
+
+  defp take_filters(params) do
+    Map.take(Map.get(params, "filter", %{}), @filters)
+  end
+
+  defp format_filters(filters) do
+    Enum.reduce(filters, [], fn filter, acc -> acc ++ do_format_filter(filter) end)
+  end
+
+  defp do_format_filter({filter, value})
+       when filter in @filters do
+    case Date.from_iso8601(value) do
+      {:ok, date} ->
+        [{filter, date}]
+
+      {:error, _} ->
+        []
+    end
+  end
+
+  defp do_format_filter(_), do: []
 end
