6d1a8e4c2291df5fb0486a6ffdee7ed22d2c374a
Maldonado <emaldonado@mbta.com>
2020-03-31 12:31:11 -0400

    feat: use data from API on ViewDisruption page

--- assets/tests/disruptions/viewDisruption.test.tsx
+++ assets/tests/disruptions/viewDisruption.test.tsx
@@ -1,39 +1,251 @@
 import { createBrowserHistory } from "history"
-import { mount } from "enzyme"
 import * as React from "react"
-import { Redirect } from "react-router"
 import { BrowserRouter } from "react-router-dom"
+import { act } from "react-dom/test-utils"
+import * as ReactDOM from "react-dom"
 
 import ViewDisruption from "../../src/disruptions/viewDisruption"
+import Adjustment from "../../src/models/adjustment"
+import DayOfWeek from "../../src/models/dayOfWeek"
+import Disruption from "../../src/models/disruption"
+import Exception from "../../src/models/exception"
 
 describe("ViewDisruption", () => {
-  test("edit link redirects to edit page", () => {
+  test("loads and displays disruption from the API", async () => {
+    const api = require("../../src/api")
+
+    jest.spyOn(api, "apiCall").mockImplementationOnce(() => {
+      return Promise.resolve(
+        new Disruption({
+          id: "1",
+          startDate: new Date("2020-01-15"),
+          endDate: new Date("2020-01-30"),
+          adjustments: [
+            new Adjustment({
+              id: "1",
+              routeId: "Green-D",
+              source: "gtfs_creator",
+              sourceLabel: "NewtonHighlandsKenmore",
+            }),
+          ],
+          daysOfWeek: [
+            new DayOfWeek({
+              id: "1",
+              startTime: "20:45:00",
+              day: "friday",
+            }),
+          ],
+          exceptions: [
+            new Exception({
+              id: "1",
+              excludedDate: new Date("2020-01-20"),
+            }),
+          ],
+          tripShortNames: [],
+        })
+      )
+    })
+
     const history = createBrowserHistory()
-    const wrapper = mount(
-      <BrowserRouter>
-        <ViewDisruption
-          match={{
-            params: { id: "foo" },
-            isExact: true,
-            path: "/disruptions/foo/edit",
-            url: "https://localhost/disruptions/foo/edit",
-          }}
-          history={history}
-          location={{
-            pathname: "/disruptions/foo/edit",
-            search: "",
-            state: {},
-            hash: "",
-          }}
-        />
-      </BrowserRouter>
+
+    let container = document.createElement("div")
+    document.body.appendChild(container)
+
+    await act(async () => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <ViewDisruption
+            match={{
+              params: { id: "1" },
+              isExact: true,
+              path: "/disruptions/1",
+              url: "https://localhost/disruptions/1",
+            }}
+            history={history}
+            location={{
+              pathname: "/disruptions/1",
+              search: "",
+              state: {},
+              hash: "",
+            }}
+          />
+        </BrowserRouter>,
+        container
+      )
+    })
+
+    expect(document.body.textContent).toMatch("NewtonHighlandsKenmore")
+    expect(document.body.textContent).toMatch("1/15/2020")
+    expect(document.body.textContent).toMatch("1/30/2020")
+    expect(document.body.textContent).toMatch("1/20/2020")
+    expect(document.body.textContent).toMatch("Friday")
+    expect(document.body.textContent).toMatch("8:45PM")
+    expect(document.body.textContent).toMatch("End of service")
+  })
+
+  test("edit link redirects to edit page", async () => {
+    const api = require("../../src/api")
+
+    jest.spyOn(api, "apiCall").mockImplementationOnce(() => {
+      return Promise.resolve(
+        new Disruption({
+          id: "1",
+          startDate: new Date("2020-01-15"),
+          endDate: new Date("2020-01-30"),
+          adjustments: [],
+          daysOfWeek: [],
+          exceptions: [],
+          tripShortNames: [],
+        })
+      )
+    })
+
+    const history = createBrowserHistory()
+
+    let container = document.createElement("div")
+    document.body.appendChild(container)
+
+    await act(async () => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <ViewDisruption
+            match={{
+              params: { id: "1" },
+              isExact: true,
+              path: "/disruptions/1",
+              url: "https://localhost/disruptions/1",
+            }}
+            history={history}
+            location={{
+              pathname: "/disruptions/1",
+              search: "",
+              state: {},
+              hash: "",
+            }}
+          />
+        </BrowserRouter>,
+        container
+      )
+    })
+
+    const editButton = container.querySelector(
+      "#edit-disruption-button"
+    ) as Element
+    expect(editButton).toBeDefined()
+    expect(editButton.textContent).toEqual("edit disruption times")
+
+    act(() => {
+      editButton.dispatchEvent(new MouseEvent("click", { bubbles: true }))
+    })
+
+    expect(location.pathname).toBe("/disruptions/1/edit")
+  })
+
+  test("handles error on fetching / parsing", async () => {
+    const api = require("../../src/api")
+
+    jest.spyOn(api, "apiCall").mockImplementationOnce(() => {
+      return Promise.resolve("error")
+    })
+
+    const history = createBrowserHistory()
+
+    let container = document.createElement("div")
+    document.body.appendChild(container)
+
+    await act(async () => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <ViewDisruption
+            match={{
+              params: { id: "1" },
+              isExact: true,
+              path: "/disruptions/1",
+              url: "https://localhost/disruptions/1",
+            }}
+            history={history}
+            location={{
+              pathname: "/disruptions/1",
+              search: "",
+              state: {},
+              hash: "",
+            }}
+          />
+        </BrowserRouter>,
+        container
+      )
+    })
+
+    expect(document.body.textContent).toMatch(
+      "Error fetching or parsing disruption."
     )
+  })
 
-    wrapper
-      .find("#edit-disruption-button")
-      .find("button")
-      .simulate("click")
+  test("handles error with day of week values", async () => {
+    const api = require("../../src/api")
 
-    expect(wrapper.exists(Redirect)).toBe(true)
+    jest.spyOn(api, "apiCall").mockImplementationOnce(() => {
+      return Promise.resolve(
+        new Disruption({
+          id: "1",
+          startDate: new Date("2020-01-15"),
+          endDate: new Date("2020-01-30"),
+          adjustments: [
+            new Adjustment({
+              id: "1",
+              routeId: "Green-D",
+              source: "gtfs_creator",
+              sourceLabel: "NewtonHighlandsKenmore",
+            }),
+          ],
+          daysOfWeek: [
+            new DayOfWeek({
+              id: "1",
+              startTime: "20:37:00",
+              day: "friday",
+            }),
+          ],
+          exceptions: [
+            new Exception({
+              id: "1",
+              excludedDate: new Date("2020-01-20"),
+            }),
+          ],
+          tripShortNames: [],
+        })
+      )
+    })
+
+    const history = createBrowserHistory()
+
+    let container = document.createElement("div")
+    document.body.appendChild(container)
+
+    await act(async () => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <ViewDisruption
+            match={{
+              params: { id: "1" },
+              isExact: true,
+              path: "/disruptions/1",
+              url: "https://localhost/disruptions/1",
+            }}
+            history={history}
+            location={{
+              pathname: "/disruptions/1",
+              search: "",
+              state: {},
+              hash: "",
+            }}
+          />
+        </BrowserRouter>,
+        container
+      )
+    })
+
+    expect(document.body.textContent).toMatch(
+      "Error parsing day of week information."
+    )
   })
 })
