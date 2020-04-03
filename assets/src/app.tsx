39efa63b0c171d9c7dd411a145bf4fd8fb9537ef
Frank LaNasa <fjlanasa@Franks-MacBook-Pro.local>
2020-04-03 09:34:53 -0400

    fix: wrap Routes in Switch component

--- assets/src/app.tsx
+++ assets/src/app.tsx
@@ -8,7 +8,7 @@ require("../css/app.scss")
 import "phoenix_html"
 import * as React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 import EditDisruption from "./disruptions/editDisruption"
 import { NewDisruption } from "./disruptions/newDisruption"
@@ -18,18 +18,24 @@ import DisruptionIndex from "./disruptions/disruptionIndex"
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <Route
-        exact={true}
-        path="/"
-        render={() => <DisruptionIndex disruptions={[]} />}
-      />
-      <Route exact={true} path="/disruptions/new" component={NewDisruption} />
-      <Route exact={true} path="/disruptions/:id" component={ViewDisruption} />
-      <Route
-        exact={true}
-        path="/disruptions/:id/edit"
-        component={EditDisruption}
-      />
+      <Switch>
+        <Route
+          exact={true}
+          path="/"
+          render={() => <DisruptionIndex disruptions={[]} />}
+        />
+        <Route exact={true} path="/disruptions/new" component={NewDisruption} />
+        <Route
+          exact={true}
+          path="/disruptions/:id"
+          component={ViewDisruption}
+        />
+        <Route
+          exact={true}
+          path="/disruptions/:id/edit"
+          component={EditDisruption}
+        />
+      </Switch>
     </BrowserRouter>
   )
 }
