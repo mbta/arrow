3878b9256300bb8efabf34502cdbdf492f0e8ab0
Cora Grant <394835+x@users.noreply.github.com>
2020-08-10 12:17:52 -0400

    chore: add auto dump for migrate/rollback tasks
    Since the `structure.sql` is now kept in sync with migrations, this also
    changes `ecto.setup` to use `ecto.load` instead of `ecto.migrate`,
    removing the need to run every migration from the beginning to arrive at
    the correct database structure.

--- mix.exs
+++ mix.exs
@@ -82,8 +82,10 @@ defmodule Arrow.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.migrate": ["ecto.migrate", "ecto.dump"],
+      "ecto.setup": ["ecto.create", "ecto.load", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "ecto.rollback": ["ecto.rollback", "ecto.dump"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
