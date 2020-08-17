89689f81033e4e976a543b99a98d224f36dffb9d
Cora Grant <394835+x@users.noreply.github.com>
2020-08-17 16:50:28 -0400

    chore: use Postgres envs in dev as well as test

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :arrow, Arrow.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
   database: "arrow_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
