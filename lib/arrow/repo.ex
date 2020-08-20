d1e6baf96bb2696a9c94ee3974c345a8d88451e3
Gabe Durazo <gdurazo@mbta.com>
2020-08-20 08:38:39 -0500

    fix: Regenerate AWS IAM auth token on connect

--- lib/arrow/repo.ex
+++ lib/arrow/repo.ex
@@ -3,39 +3,23 @@ defmodule Arrow.Repo do
     otp_app: :arrow,
     adapter: Ecto.Adapters.Postgres
 
-  @default_pool_size 10
+  require Logger
 
-  def init(_context, config) do
-    pool_size_env = System.get_env("DATABASE_POOL_SIZE")
-    pool_size_env = if pool_size_env, do: String.to_integer(pool_size_env)
-    pool_size = pool_size_env || Keyword.get(config, :pool_size) || @default_pool_size
-    config = Keyword.put(config, :pool_size, pool_size)
+  @doc """
+  Set via the `:configure` option in the Arrow.Repo configuration, a function
+  invoked prior to each DB connection. `config` is the configured connection values
+  and it returns a new set of config values to be used when connecting.
+  """
+  def before_connect(config) do
+    :ok = Logger.info("generating_aws_rds_iam_auth_token")
+    username = Keyword.fetch!(config, :username)
+    hostname = Keyword.fetch!(config, :hostname)
+    port = Keyword.fetch!(config, :port)
 
-    config =
-      if Keyword.take(config, [:database, :username, :password, :hostname]) == [] do
-        username = System.get_env("DATABASE_USER")
-        database = System.get_env("DATABASE_NAME")
-        hostname = System.get_env("DATABASE_HOST")
-        port = "DATABASE_PORT" |> System.get_env() |> String.to_integer()
+    mod = Application.get_env(:arrow, :aws_rds_mod)
+    token = apply(mod, :generate_db_auth_token, [hostname, username, port, %{}])
+    :ok = Logger.info("generated_aws_rds_iam_auth_token")
 
-        if !username, do: raise("missing DATABASE_USER environment variable")
-        if !database, do: raise("missing DATABASE_NAME environment variable")
-        if !hostname, do: raise("missing DATABASE_HOST environment variable")
-
-        mod = Application.get_env(:arrow, :aws_rds_mod)
-        token = apply(mod, :generate_db_auth_token, [hostname, username, port, %{}])
-
-        Keyword.merge(config,
-          database: database,
-          username: username,
-          hostname: hostname,
-          password: token,
-          port: port
-        )
-      else
-        config
-      end
-
-    {:ok, config}
+    Keyword.put(config, :password, token)
   end
 end
