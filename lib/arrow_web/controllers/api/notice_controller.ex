3de061b01fe43e8c74f129abdafb36c6ab2007af
fjlanasa@gmail.com <fjlanasa@gmail.com>
2020-09-30 12:01:21 -0400

    pull controller logic into helper function

--- lib/arrow_web/controllers/api/notice_controller.ex
+++ lib/arrow_web/controllers/api/notice_controller.ex
@@ -4,42 +4,34 @@ defmodule ArrowWeb.API.NoticeController do
 
   @spec publish(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def publish(conn, params) do
+    handle(conn, params, &Arrow.DisruptionRevision.publish!/1, "marking_revisions_published")
+  end
+
+  @spec ready(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def ready(conn, params) do
+    handle(conn, params, &Arrow.DisruptionRevision.ready!/1, "marking_revisions_ready", 204)
+  end
+
+  @spec handle(Plug.Conn.t(), map(), function(), String.t(), integer()) ::
+          Plug.Conn.t()
+  defp handle(conn, params, update_fn, notice_event, success_status \\ 200) do
     try do
       revision_ids =
         params["revision_ids"]
         |> String.split(",")
         |> Enum.map(&String.to_integer/1)
 
-      :ok = Arrow.DisruptionRevision.publish!(revision_ids)
+      :ok = update_fn.(revision_ids)
 
-      :ok = Logger.info("marking_revisions_published revision_ids=#{params["revision_ids"]}")
+      :ok = Logger.info("#{notice_event} revision_ids=#{params["revision_ids"]}")
 
-      send_resp(conn, 200, "")
+      send_resp(conn, success_status, "")
     rescue
       ArgumentError ->
         send_resp(conn, 400, "bad argument")
 
       Arrow.Disruption.Error.PublishedAfterReady ->
         send_resp(conn, 400, "can't publish revision more recent than ready revision")
-    end
-  end
-
-  @spec ready(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def ready(conn, params) do
-    try do
-      revision_ids =
-        params["revision_ids"]
-        |> String.split(",")
-        |> Enum.map(&String.to_integer/1)
-
-      :ok = Arrow.DisruptionRevision.ready!(revision_ids)
-
-      :ok = Logger.info("marking_revisions_ready revision_ids=#{params["revision_ids"]}")
-
-      send_resp(conn, 204, "")
-    rescue
-      ArgumentError ->
-        send_resp(conn, 400, "bad argument")
 
       Arrow.Disruption.Error.ReadyNotLatest ->
         send_resp(conn, 400, "can't ready revision more recent than latest revision")
