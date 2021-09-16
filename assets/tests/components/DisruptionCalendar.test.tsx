496ce1193250fcf1f6abfc9ebd40901d2451f9de
Cora Grant <394835+x@users.noreply.github.com>
2021-09-16 11:58:25 -0400

    refactor: create FullCalendar events on the server
    `DisruptionCalendar` was the last place the frontend still used
    JSON:API, as a means to get the disruption data from the server into
    React to then generate the FullCalendar events. By instead generating
    the events on the server and more-or-less rendering the `FullCalendar`
    component directly, we can delete a large amount of bespoke JSON:API
    code and even cut a dependency (`rrule`) from the frontend.
    
    For the moment, the backend still needs JSON:API to support the index
    endpoint used by `gtfs_creator` and the `copy_db` mix task.

--- assets/tests/components/DisruptionCalendar.test.tsx
+++ assets/tests/components/DisruptionCalendar.test.tsx
@@ -1,330 +1,59 @@
 import React from "react"
-import DisruptionCalendar, {
-  disruptionsToCalendarEvents,
-} from "../../src/components/DisruptionCalendar"
+import DisruptionCalendar from "../../src/components/DisruptionCalendar"
 import { render } from "@testing-library/react"
-import DisruptionRevision from "../../src/models/disruptionRevision"
-import Adjustment from "../../src/models/adjustment"
-import DayOfWeek from "../../src/models/dayOfWeek"
-import Exception from "../../src/models/exception"
-import { toUTCDate } from "../../src/jsonApi"
-
-const SAMPLE_DISRUPTIONS = [
-  new DisruptionRevision({
-    id: "1",
-    disruptionId: "1",
-    startDate: toUTCDate("2019-10-31"),
-    endDate: toUTCDate("2019-11-15"),
-    isActive: true,
-    adjustments: [
-      new Adjustment({
-        id: "1",
-        routeId: "Red",
-        sourceLabel: "AlewifeHarvard",
-      }),
-    ],
-    daysOfWeek: [
-      new DayOfWeek({
-        id: "1",
-        startTime: "20:45:00",
-        dayName: "friday",
-      }),
-      new DayOfWeek({
-        id: "2",
-        dayName: "saturday",
-      }),
-      new DayOfWeek({
-        id: "3",
-        dayName: "sunday",
-      }),
-    ],
-    exceptions: [new Exception({ excludedDate: toUTCDate("2019-11-01") })],
-    tripShortNames: [],
-  }),
-  new DisruptionRevision({
-    id: "3",
-    disruptionId: "3",
-    startDate: toUTCDate("2019-11-15"),
-    endDate: toUTCDate("2019-11-30"),
-    isActive: true,
-    adjustments: [
-      new Adjustment({
-        id: "2",
-        routeId: "Green-D",
-        sourceLabel: "Kenmore-Newton Highlands",
-      }),
-    ],
-    daysOfWeek: [
-      new DayOfWeek({
-        id: "1",
-        startTime: "20:45:00",
-        dayName: "friday",
-      }),
-      new DayOfWeek({
-        id: "2",
-        dayName: "saturday",
-      }),
-      new DayOfWeek({
-        id: "3",
-        dayName: "sunday",
-      }),
-    ],
-    exceptions: [],
-    tripShortNames: [],
-  }),
-  new DisruptionRevision({
-    id: "4",
-    disruptionId: "4",
-    startDate: toUTCDate("2019-11-15"),
-    endDate: toUTCDate("2019-11-30"),
-    isActive: true,
-    adjustments: [
-      new Adjustment({
-        id: "2",
-        routeId: "Green-D",
-        sourceLabel: "Kenmore-Newton Highlands",
-      }),
-    ],
-    daysOfWeek: [],
-    exceptions: [],
-    tripShortNames: [],
-  }),
-]
 
 describe("DisruptionCalendar", () => {
-  describe("disruptionsToCalendarEvents", () => {
-    it("parses disruptions", () => {
-      expect(disruptionsToCalendarEvents(SAMPLE_DISRUPTIONS)).toEqual([
-        {
-          id: "1",
-          title: "AlewifeHarvard",
-          backgroundColor: "#da291c",
-          start: "2019-11-02",
-          end: "2019-11-04",
-          url: "/disruptions/1",
-          eventDisplay: "block",
-          allDay: true,
-        },
-        {
-          id: "1",
-          title: "AlewifeHarvard",
-          backgroundColor: "#da291c",
-          start: "2019-11-08",
-          end: "2019-11-11",
-          url: "/disruptions/1",
-          eventDisplay: "block",
-          allDay: true,
-        },
-        {
-          id: "1",
-          title: "AlewifeHarvard",
-          backgroundColor: "#da291c",
-          start: "2019-11-15",
-          end: "2019-11-15",
-          url: "/disruptions/1",
-          eventDisplay: "block",
-          allDay: true,
-        },
-        {
-          id: "3",
-          title: "Kenmore-Newton Highlands",
-          backgroundColor: "#00843d",
-          start: "2019-11-15",
-          end: "2019-11-18",
-          url: "/disruptions/3",
-          eventDisplay: "block",
-          allDay: true,
-        },
-        {
-          id: "3",
-          title: "Kenmore-Newton Highlands",
-          backgroundColor: "#00843d",
-          start: "2019-11-22",
-          end: "2019-11-25",
-          url: "/disruptions/3",
-          eventDisplay: "block",
-          allDay: true,
-        },
-        {
-          id: "3",
-          title: "Kenmore-Newton Highlands",
-          backgroundColor: "#00843d",
-          start: "2019-11-29",
-          end: "2019-12-01",
-          url: "/disruptions/3",
-          eventDisplay: "block",
-          allDay: true,
-        },
-      ])
-    })
-
-    it("handles invalid days of week gracefully", () => {
-      expect(
-        disruptionsToCalendarEvents([
-          new DisruptionRevision({
-            id: "1",
-            disruptionId: "1",
-            startDate: toUTCDate("2020-07-01"),
-            endDate: toUTCDate("2020-07-02"),
-            isActive: true,
-            adjustments: [
-              new Adjustment({
-                id: "1",
-                routeId: "Red",
-                sourceLabel: "AlewifeHarvard",
-              }),
-            ],
-            daysOfWeek: [
-              new DayOfWeek({
-                id: "1",
-                startTime: "20:45:00",
-                dayName: "friday",
-              }),
-            ],
-            exceptions: [],
-            tripShortNames: [],
-          }),
-        ])
-      ).toEqual([])
-    })
-  })
-
   test("renders correctly", () => {
-    const tree = render(
-      <DisruptionCalendar
-        initialDate={toUTCDate("2019-11-15")}
-        data={SAMPLE_DISRUPTIONS}
-      />
-    )
-    expect(tree).toMatchSnapshot()
-  })
-
-  test("accepts a JSON:API disruptions response as input", () => {
-    const calendar = render(
-      <DisruptionCalendar
-        initialDate={toUTCDate("2021-01-01")}
-        data={{
-          data: [
-            {
-              attributes: {},
-              id: "1",
-              relationships: {
-                revisions: { data: [{ id: "1", type: "disruption_revision" }] },
-                ready_revision: { data: null },
-                published_revision: { data: null },
-              },
-              type: "disruption",
-            },
-          ],
-          included: [
-            {
-              attributes: {
-                end_date: "2021-01-01",
-                start_date: "2021-01-01",
-                is_active: true,
-              },
-              id: "1",
-              relationships: {
-                adjustments: {
-                  data: [{ id: "1", type: "adjustment" }],
-                },
-                days_of_week: { data: [{ id: "1", type: "day_of_week" }] },
-                exceptions: { data: [] },
-                trip_short_names: { data: [] },
-              },
-              type: "disruption_revision",
-            },
-            {
-              attributes: { day_name: "friday" },
-              id: "1",
-              type: "day_of_week",
-            },
-            {
-              attributes: {
-                route_id: "Green-D",
-                source: "gtfs_creator",
-                source_label: "KenmoreReservoir",
-              },
-              id: "1",
-              type: "adjustment",
-            },
-          ],
-          jsonapi: { version: "1.0" },
-        }}
-      />
-    )
-
-    expect(calendar.queryByText(/error loading/i)).not.toBeInTheDocument()
-
-    expect(
-      calendar.container.querySelector(
-        '[data-date="2021-01-01"] .fc-daygrid-event'
-      )
-    ).not.toBeNull()
-  })
-
-  test("displays an error if the input is not valid JSON:API data", () => {
-    const calendar = render(
-      <DisruptionCalendar data={{ this_data: "is not JSON:API" }} />
-    )
-
-    expect(calendar.queryByText(/error loading/i)).toBeInTheDocument()
-  })
-
-  test("handles daylight savings correctly", () => {
     const { container } = render(
       <DisruptionCalendar
-        initialDate={toUTCDate("2020-11-15")}
-        data={[
-          new DisruptionRevision({
-            id: "1",
-            disruptionId: "1",
-            startDate: toUTCDate("2020-10-30"),
-            endDate: toUTCDate("2020-11-22"),
-            isActive: true,
-            adjustments: [
-              new Adjustment({
-                id: "1",
-                routeId: "Red",
-                sourceLabel: "AlewifeHarvard",
-              }),
-            ],
-            daysOfWeek: [
-              new DayOfWeek({
-                id: "1",
-                startTime: "20:45:00",
-                dayName: "friday",
-              }),
-              new DayOfWeek({
-                id: "2",
-                dayName: "saturday",
-              }),
-              new DayOfWeek({
-                id: "3",
-                dayName: "sunday",
-              }),
-            ],
-            exceptions: [
-              new Exception({ excludedDate: toUTCDate("2020-11-15") }),
-            ],
-            tripShortNames: [],
-          }),
+        initialDate="2019-11-15"
+        events={[
+          {
+            title: "AlewifeHarvard",
+            className: "route-Red",
+            start: "2019-11-02",
+            end: "2019-11-04",
+            url: "/disruptions/1",
+          },
+          {
+            title: "AlewifeHarvard",
+            className: "route-Red",
+            start: "2019-11-08",
+            end: "2019-11-11",
+            url: "/disruptions/1",
+          },
+          {
+            title: "AlewifeHarvard",
+            className: "route-Red",
+            start: "2019-11-15",
+            end: "2019-11-15",
+            url: "/disruptions/1",
+          },
+          {
+            title: "Kenmore-Newton Highlands",
+            className: "route-Green-D",
+            start: "2019-11-15",
+            end: "2019-11-18",
+            url: "/disruptions/3",
+          },
+          {
+            title: "Kenmore-Newton Highlands",
+            className: "route-Green-D",
+            start: "2019-11-22",
+            end: "2019-11-25",
+            url: "/disruptions/3",
+          },
+          {
+            title: "Kenmore-Newton Highlands",
+            className: "route-Green-D",
+            start: "2019-11-29",
+            end: "2019-12-01",
+            url: "/disruptions/3",
+          },
         ]}
       />
     )
 
-    const activeDays = ["01", "06", "08", "13", "20", "22"]
-
-    activeDays.forEach((day) => {
-      expect(
-        container.querySelector(
-          `[data-date="2020-11-${day}"] .fc-daygrid-event`
-        )
-      ).not.toBeNull()
-    })
-
-    expect(
-      container.querySelector('[data-date="2020-11-15"] .fc-daygrid-event')
-    ).toBeNull()
+    expect(container).toMatchSnapshot()
   })
 })
---
+++ test/arrow_web/views/disruption_view/calendar_test.exs
@@ -0,0 +1,112 @@
+defmodule ArrowWeb.DisruptionView.CalendarTest do
+  use ExUnit.Case, async: true
+
+  alias Arrow.{Adjustment, Disruption, DisruptionRevision}
+  alias Arrow.Disruption.{DayOfWeek, Exception}
+  alias ArrowWeb.DisruptionView.Calendar, as: DCalendar
+
+  describe "props/1" do
+    test "converts a list of Disruptions to DisruptionCalendar props" do
+      disruption = %Disruption{
+        id: 123,
+        revisions: [
+          %DisruptionRevision{
+            start_date: ~D[2021-01-01],
+            end_date: ~D[2021-01-31],
+            adjustments: [
+              %Adjustment{route_id: "Blue", source_label: "Wonderland"},
+              %Adjustment{route_id: "Orange", source_label: "Wellington"}
+            ],
+            days_of_week: [
+              %DayOfWeek{day_name: "monday", start_time: ~T[20:45:00], end_time: nil},
+              %DayOfWeek{day_name: "tuesday", start_time: ~T[20:45:00], end_time: nil}
+            ],
+            exceptions: [%Exception{excluded_date: ~D[2021-01-11]}]
+          }
+        ]
+      }
+
+      expected_events = [
+        %{
+          title: "Wonderland",
+          classNames: "route-Blue",
+          start: ~D[2021-01-04],
+          end: ~D[2021-01-06],
+          url: "/disruptions/123"
+        },
+        %{
+          title: "Wonderland",
+          classNames: "route-Blue",
+          start: ~D[2021-01-12],
+          end: ~D[2021-01-13],
+          url: "/disruptions/123"
+        },
+        %{
+          title: "Wonderland",
+          classNames: "route-Blue",
+          start: ~D[2021-01-18],
+          end: ~D[2021-01-20],
+          url: "/disruptions/123"
+        },
+        %{
+          title: "Wonderland",
+          classNames: "route-Blue",
+          start: ~D[2021-01-25],
+          end: ~D[2021-01-27],
+          url: "/disruptions/123"
+        },
+        %{
+          title: "Wellington",
+          classNames: "route-Orange",
+          start: ~D[2021-01-04],
+          end: ~D[2021-01-06],
+          url: "/disruptions/123"
+        },
+        %{
+          title: "Wellington",
+          classNames: "route-Orange",
+          start: ~D[2021-01-12],
+          end: ~D[2021-01-13],
+          url: "/disruptions/123"
+        },
+        %{
+          title: "Wellington",
+          classNames: "route-Orange",
+          start: ~D[2021-01-18],
+          end: ~D[2021-01-20],
+          url: "/disruptions/123"
+        },
+        %{
+          title: "Wellington",
+          classNames: "route-Orange",
+          start: ~D[2021-01-25],
+          end: ~D[2021-01-27],
+          url: "/disruptions/123"
+        }
+      ]
+
+      assert DCalendar.props([disruption]) == %{events: expected_events}
+    end
+
+    test "returns no events when passed no disruptions" do
+      assert DCalendar.props([]) == %{events: []}
+    end
+
+    test "returns no events for disruptions with no adjustments" do
+      disruption = %Disruption{
+        id: 123,
+        revisions: [
+          %DisruptionRevision{
+            start_date: ~D[2021-01-01],
+            end_date: ~D[2021-01-31],
+            adjustments: [],
+            days_of_week: [%DayOfWeek{day_name: "monday", start_time: nil, end_time: nil}],
+            exceptions: []
+          }
+        ]
+      }
+
+      assert DCalendar.props([disruption]) == %{events: []}
+    end
+  end
+end
