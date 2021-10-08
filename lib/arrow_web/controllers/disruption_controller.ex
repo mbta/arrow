0f78040a493097acdf7a28523da9f2b5dba6493c
Cora Grant <394835+x@users.noreply.github.com>
2021-10-08 16:57:49 -0400

    refactor: add specs to disruption controller

--- lib/arrow_web/controllers/disruption_controller.ex
+++ lib/arrow_web/controllers/disruption_controller.ex
@@ -5,22 +5,27 @@ defmodule ArrowWeb.DisruptionController do
   alias Arrow.{Adjustment, Disruption, DisruptionRevision}
   alias ArrowWeb.ErrorHelpers
   alias Ecto.Changeset
+  alias Plug.Conn
 
+  @spec index(Conn.t(), Conn.params()) :: Conn.t()
   def index(conn, params) do
     filters = Filters.from_params(params)
     render(conn, "index.html", disruptions: Index.all(filters), filters: filters)
   end
 
+  @spec show(Conn.t(), Conn.params()) :: Conn.t()
   def show(conn, %{"id" => id}) do
     %{id: id, revisions: [revision]} = Disruption.get!(id)
     render(conn, "show.html", id: id, revision: revision)
   end
 
+  @spec new(Conn.t(), Conn.params()) :: Conn.t()
   def new(conn, _params) do
     changeset = DisruptionRevision.new() |> Changeset.change()
     render(conn, "new.html", adjustments: Adjustment.all(), changeset: changeset)
   end
 
+  @spec edit(Conn.t(), Conn.params()) :: Conn.t()
   def edit(conn, %{"id" => id}) do
     %{revisions: [revision]} = Disruption.get!(id)
 
@@ -31,6 +36,7 @@ defmodule ArrowWeb.DisruptionController do
     )
   end
 
+  @spec create(Conn.t(), Conn.params()) :: Conn.t()
   def create(conn, %{"revision" => attrs}) do
     case Disruption.create(attrs) do
       {:ok, %{disruption_id: id}} ->
@@ -45,6 +51,7 @@ defmodule ArrowWeb.DisruptionController do
     end
   end
 
+  @spec update(Conn.t(), Conn.params()) :: Conn.t()
   def update(conn, %{"id" => id, "revision" => attrs}) do
     case Disruption.update(id, put_new_assocs(attrs)) do
       {:ok, _revision} ->
@@ -59,17 +66,20 @@ defmodule ArrowWeb.DisruptionController do
     end
   end
 
+  @spec delete(Conn.t(), Conn.params()) :: Conn.t()
   def delete(conn, %{"id" => id}) do
     _revision = Disruption.delete!(id)
     redirect(conn, to: Routes.disruption_path(conn, :show, id))
   end
 
+  @spec errors(Changeset.t()) :: [String.t()]
   defp errors(changeset) do
     changeset
     |> Changeset.traverse_errors(&ErrorHelpers.translate_error/1)
     |> ErrorHelpers.flatten_errors()
   end
 
+  @spec put_new_assocs(%{optional(binary) => any}) :: %{binary => any}
   defp put_new_assocs(attrs) do
     # Ensure a form submission with no instances of an association is interpreted as "delete all
     # existing records" rather than "leave existing records alone"
