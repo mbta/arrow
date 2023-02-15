b4372d38734e8826decaa34517b29b29d26b8a76
Brian Fauble <bfauble@users.noreply.github.com>
2023-02-15 13:51:36 -0600

    only get cognito_client_secret from env when running in prod mode

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,10 +2,10 @@ import Config
 
 if config_env() == :prod do
   config :arrow, ArrowWeb.Endpoint, secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
-end
 
-config :ueberauth, Ueberauth,
+  config :ueberauth, Ueberauth,
   providers: [
     cognito:
       {Ueberauth.Strategy.Cognito, [client_secret: System.get_env("COGNITO_CLIENT_SECRET")]}
   ]
+end
