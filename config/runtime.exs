af95fbc8fa19647f7b416185731f9796c07be73c
Nick Stein <nstein@mbta.com>
2023-03-01 18:39:48 -0500

    chore: add runtime config for sentry

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,24 @@
 import Config
 
 if config_env() == :prod do
+  sentry_env = System.get_env("SENTRY_ENV")
+
+  if not is_nil(sentry_env) do
+    config :sentry,
+      dsn: System.fetch_env!("SENTRY_DSN"),
+      environment_name: sentry_env,
+      enable_source_code_context: true,
+      root_source_code_path: File.cwd!(),
+      tags: %{
+        env: sentry_env
+      },
+      included_environments: [sentry_env]
+
+    config :logger, Sentry.LoggerBackend,
+      level: :warn,
+      capture_log_messages: true
+  end
+
   config :arrow, ArrowWeb.Endpoint, secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
 
   config :ueberauth, Ueberauth,
