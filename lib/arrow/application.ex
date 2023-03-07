4d33edd8c547b1992d416438968d43248b365066
Nick Stein <nstein@mbta.com>
2023-03-06 20:37:22 -0500

    wip: move invocation to config

--- lib/arrow/application.ex
+++ lib/arrow/application.ex
@@ -6,9 +6,6 @@ defmodule Arrow.Application do
   use Application
 
   def start(_type, _args) do
-    # Invoke Sentry logger:
-    _ = Logger.add_backend(Sentry.LoggerBackend)
-
     run_adjustment_fetcher? = Application.get_env(:arrow, :fetch_adjustments?)
     run_migrations_at_startup? = Application.get_env(:arrow, :run_migrations_at_startup?)
 
