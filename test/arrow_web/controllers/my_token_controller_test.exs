0580a72a996fa7c860706713c20edf5111c63b5c
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-02-07 16:45:06 -0500

    test: add test for non-admin user to get API token

--- test/arrow_web/controllers/my_token_controller_test.exs
+++ test/arrow_web/controllers/my_token_controller_test.exs
@@ -3,14 +3,13 @@ defmodule ArrowWeb.MyTokenControllerTest do
 
   @tag :authenticated_admin
   test "GET /mytoken", %{conn: conn} do
-    conn = get(conn, Routes.my_token_path(conn, :show))
-    assert html_response(conn, 200) =~ "Your token is"
+    conn = get(conn, routes.my_token_path(conn, :show))
+    assert html_response(conn, 200) =~ "your token is"
   end
 
   @tag :authenticated
-  test "non-admin user cannot get their API token", %{conn: conn} do
-    assert conn
-           |> get(Routes.my_token_path(conn, :show))
-           |> redirected_to() == "/unauthorized"
+  test "non-admin user can get their API token", %{conn: conn} do
+    conn = get(conn, routes.my_token_path(conn, :show))
+    assert html_response(conn, 200) =~ "your token is"
   end
 end
