2916ce2c8dd6f5e8749366e25499e424cb98cc86
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-02-07 17:16:37 -0500

    test: update GETs in tests to reflect non-admin update

--- test/arrow_web/controllers/my_token_controller_test.exs
+++ test/arrow_web/controllers/my_token_controller_test.exs
@@ -3,13 +3,13 @@ defmodule ArrowWeb.MyTokenControllerTest do
 
   @tag :authenticated_admin
   test "GET /mytoken", %{conn: conn} do
-    conn = get(conn, routes.my_token_path(conn, :show))
-    assert html_response(conn, 200) =~ "your token is"
+    conn = get(conn, Routes.my_token_path(conn, :show))
+    assert html_response(conn, 200) =~ "Your token is"
   end
 
   @tag :authenticated
   test "non-admin user can get their API token", %{conn: conn} do
-    conn = get(conn, routes.my_token_path(conn, :show))
-    assert html_response(conn, 200) =~ "your token is"
+    conn = get(conn, Routes.my_token_path(conn, :show))
+    assert html_response(conn, 200) =~ "Your token is"
   end
 end
