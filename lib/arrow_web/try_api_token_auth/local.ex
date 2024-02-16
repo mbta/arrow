1cabc77204da53d0b1f88c2233a338b47eaeb1d3
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-02-16 10:39:51 -0500

    fix: remove extra check of email domain on local token login

--- lib/arrow_web/try_api_token_auth/local.ex
+++ lib/arrow_web/try_api_token_auth/local.ex
@@ -8,23 +8,12 @@ defmodule ArrowWeb.TryApiTokenAuth.Local do
 
   @spec sign_in(Conn.t(), Arrow.AuthToken.t()) :: Conn.t()
   def sign_in(%Conn{} = conn, %Arrow.AuthToken{} = auth_token) do
-    if String.ends_with?(
-         auth_token.username,
-         Application.get_env(:arrow, :local_token_allowed_domain)
-       ) do
-      conn
-      |> Guardian.Plug.sign_in(
-        ArrowWeb.AuthManager,
-        auth_token.username,
-        %{roles: ["read-only"]},
-        ttl: {0, :second}
-      )
-    else
-      Logger.warn(
-        "refusing to login in API client username=#{inspect(auth_token.username)} reason=unexpected username"
-      )
-
-      conn
-    end
+    conn
+    |> Guardian.Plug.sign_in(
+      ArrowWeb.AuthManager,
+      auth_token.username,
+      %{roles: ["read-only"]},
+      ttl: {0, :second}
+    )
   end
 end
--- test/arrow_web/try_api_token_auth/local_test.exs
+++ test/arrow_web/try_api_token_auth/local_test.exs
@@ -29,18 +29,6 @@ defmodule ArrowWeb.TryApiTokenAuth.LocalTest do
 
       refute Guardian.Plug.authenticated?(conn)
     end
-
-    test "does not sign in a token user who does not have an @mbta.com email", %{conn: conn} do
-      unknown_domain = "arrow-missing@unknown.com"
-      Arrow.AuthToken.get_or_create_token_for_user(unknown_domain)
-      auth_token = auth_token_for(unknown_domain)
-
-      {conn, log} = with_log(fn -> Local.sign_in(conn, auth_token) end)
-
-      refute Guardian.Plug.authenticated?(conn)
-
-      assert log =~ "refusing to login"
-    end
   end
 
   defp auth_token_for(username) do
