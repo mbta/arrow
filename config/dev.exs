ab09a2831031900fc01c999b8b7e562fb5dd98c9
Paul Swartz <paul@paulswartz.net>
2024-03-01 09:40:29 -0500

    feat: include HTTPS support for local development

--- config/dev.exs
+++ config/dev.exs
@@ -17,6 +17,12 @@ config :arrow, Arrow.Repo,
 # with webpack to recompile .js and .css sources.
 config :arrow, ArrowWeb.Endpoint,
   http: [port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    keyfile: "priv/cert/selfsigned_key.pem",
+    certfile: "priv/cert/selfsigned.pem"
+  ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
