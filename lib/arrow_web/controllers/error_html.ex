6ce0390e6d9602682b2761561ece6af74d2109f3
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-06-03 16:35:36 -0400

    tests: fix error_view by replacing Phoenix.View with Phoenix.Template

--- lib/arrow_web/controllers/error_html.ex
+++ lib/arrow_web/controllers/error_html.ex
@@ -2,18 +2,14 @@ defmodule ArrowWeb.ErrorView do
   use ArrowWeb, :html
   alias JaSerializer.ErrorSerializer
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
-  end
+  # If you want to customize your error pages,
+  # uncomment the embed_templates/1 call below
+  # and add pages to the error directory:
+  #
+  #   * lib/<%= @lib_web_name %>/controllers/error/404.html.heex
+  #   * lib/<%= @lib_web_name %>/controllers/error/500.html.heex
+  #
+  # embed_templates "error/*"
 
   def render("404.json" <> _, _assigns) do
     ErrorSerializer.format(%{
@@ -22,5 +18,12 @@ defmodule ArrowWeb.ErrorView do
       status: "404",
       title: "Resource Not Found"
     })
+  end
+
+  # The default is to render a plain text page based on
+  # the template name. For example, "404.html" becomes
+  # "Not Found".
+  def render(template, _assigns) do
+    Phoenix.Controller.status_message_from_template(template)
   end
 end
