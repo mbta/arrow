2acaa836d645558ce4b21692405b504d148a2f59
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-06-05 16:44:48 -0400

    tests: add authentication to controller tests

--- test/arrow_web/controllers/shape_controller_test.exs
+++ test/arrow_web/controllers/shape_controller_test.exs
@@ -1,5 +1,5 @@
 defmodule ArrowWeb.ShapeControllerTest do
-  use ArrowWeb.ConnCase
+  use ArrowWeb.ConnCase, async: true
 
   import Arrow.ShuttleFixtures
 
@@ -8,6 +8,7 @@ defmodule ArrowWeb.ShapeControllerTest do
   @invalid_attrs %{name: nil}
 
   describe "index" do
+    @tag :authenticated_admin
     test "lists all shapes", %{conn: conn} do
       conn = get(conn, ~p"/shapes")
       assert html_response(conn, 200) =~ "Listing Shapes"
@@ -15,6 +16,7 @@ defmodule ArrowWeb.ShapeControllerTest do
   end
 
   describe "new shape" do
+    @tag :authenticated_admin
     test "renders form", %{conn: conn} do
       conn = get(conn, ~p"/shapes/new")
       assert html_response(conn, 200) =~ "New Shape"
@@ -22,16 +24,19 @@ defmodule ArrowWeb.ShapeControllerTest do
   end
 
   describe "create shape" do
+    @tag :authenticated_admin
     test "redirects to show when data is valid", %{conn: conn} do
       conn = post(conn, ~p"/shapes", shape: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == ~p"/shapes/#{id}"
 
+      conn = ArrowWeb.ConnCase.authenticated_admin()
       conn = get(conn, ~p"/shapes/#{id}")
       assert html_response(conn, 200) =~ "Shape #{id}"
     end
 
+    @tag :authenticated_admin
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, ~p"/shapes", shape: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Shape"
@@ -41,6 +46,7 @@ defmodule ArrowWeb.ShapeControllerTest do
   describe "edit shape" do
     setup [:create_shape]
 
+    @tag :authenticated_admin
     test "renders form for editing chosen shape", %{conn: conn, shape: shape} do
       conn = get(conn, ~p"/shapes/#{shape}/edit")
       assert html_response(conn, 200) =~ "Edit Shape"
@@ -50,14 +56,17 @@ defmodule ArrowWeb.ShapeControllerTest do
   describe "update shape" do
     setup [:create_shape]
 
+    @tag :authenticated_admin
     test "redirects when data is valid", %{conn: conn, shape: shape} do
       conn = put(conn, ~p"/shapes/#{shape}", shape: @update_attrs)
       assert redirected_to(conn) == ~p"/shapes/#{shape}"
 
+      conn = ArrowWeb.ConnCase.authenticated_admin()
       conn = get(conn, ~p"/shapes/#{shape}")
       assert html_response(conn, 200) =~ "some updated name"
     end
 
+    @tag :authenticated_admin
     test "renders errors when data is invalid", %{conn: conn, shape: shape} do
       conn = put(conn, ~p"/shapes/#{shape}", shape: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit Shape"
@@ -67,9 +76,12 @@ defmodule ArrowWeb.ShapeControllerTest do
   describe "delete shape" do
     setup [:create_shape]
 
+    @tag :authenticated_admin
     test "deletes chosen shape", %{conn: conn, shape: shape} do
       conn = delete(conn, ~p"/shapes/#{shape}")
       assert redirected_to(conn) == ~p"/shapes"
+
+      conn = ArrowWeb.ConnCase.authenticated_admin()
 
       assert_error_sent 404, fn ->
         get(conn, ~p"/shapes/#{shape}")
