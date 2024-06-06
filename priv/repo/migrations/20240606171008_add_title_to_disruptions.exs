0ff59fdab6012cdff8e80ecd9da7879ced2953b2
Brian Fauble <bfauble@users.noreply.github.com>
2024-06-06 11:51:51 -0600

    add migration and default title to the description

---
+++ priv/repo/migrations/20240606171008_add_title_to_disruptions.exs
@@ -0,0 +1,25 @@
+defmodule Arrow.Repo.Migrations.AddTitleToDisruptions do
+  use Ecto.Migration
+  import Ecto.Query
+
+  def up do
+    alter table(:disruption_revisions) do
+      add :title, :string, size: 40
+    end
+
+    flush()
+
+    from(d in Arrow.DisruptionRevision, update: [set: [title: fragment("substring(?, 1, 40)", d.description)]])
+    |> Arrow.Repo.update_all([])
+
+    alter table(:disruption_revisions) do
+      modify :title, :string, size: 40, null: false
+    end
+  end
+
+  def down do
+    alter table(:disruption_revisions) do
+      remove :title
+    end
+  end
+end
