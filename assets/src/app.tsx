cf7ee900029e97e8a77a0afe331815eb129c0833
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-06-06 18:23:39 -0400

    fix: update esbuild, remove css refs and add empty loader

--- assets/src/app.tsx
+++ assets/src/app.tsx
@@ -1,8 +1,3 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-declare function require(name: string): string
-
 import "phoenix_html"
 
 import ReactPhoenix from "./ReactPhoenix"
