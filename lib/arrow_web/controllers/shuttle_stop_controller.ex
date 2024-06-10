6ef9af5892efc53ee9ac8d33c6f0b23e6f84c268
Brian Fauble <bfauble@users.noreply.github.com>
2024-06-10 12:58:47 -0600

    add controller and routes for shuttle_stops resource minus show and delete actions

---
+++ lib/arrow_web/controllers/shuttle_stop_controller.ex
@@ -0,0 +1,30 @@
+defmodule ArrowWeb.ShuttleStopController do
+  use ArrowWeb, :controller
+
+  alias Plug.Conn
+
+  @spec index(Conn.t(), Conn.params()) :: Conn.t()
+  def index(%{assigns: %{current_user: user}} = conn, _params) do
+    render(conn, "index.html", [])
+  end
+
+  @spec new(Conn.t(), Conn.params()) :: Conn.t()
+  def new(conn, _params) do
+    render(conn, "new.html", [])
+  end
+
+  @spec create(Conn.t(), Conn.params()) :: Conn.t()
+  def create(conn, _params) do
+    render(conn, "new.html", [])
+  end
+
+  @spec edit(Conn.t(), Conn.params()) :: Conn.t()
+  def edit(conn, _parmas) do
+    render(conn, "edit.html", [])
+  end
+
+  @spec update(Conn.t(), Conn.params()) :: Conn.t()
+  def update(conn, %{"id" => _id} = _params) do
+    render(conn, "edit.html", [])
+  end
+end
