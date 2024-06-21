303a9e950d2b6fc5337aefec9b96d40c2f04d4dc
Nick Stein <nstein@mbta.com>
2024-06-21 11:27:10 -0400

    feat: add s3 upload functionality

--- lib/arrow/shuttle.ex
+++ lib/arrow/shuttle.ex
@@ -4,6 +4,7 @@ defmodule Arrow.Shuttle do
   """
 
   import Ecto.Query, warn: false
+
   alias Arrow.Repo
 
   alias Arrow.Shuttle.Shape
@@ -50,9 +51,76 @@ defmodule Arrow.Shuttle do
 
   """
   def create_shape(attrs \\ %{}) do
+    if Map.has_key?(attrs, "filename") do
+      case upload_shape(attrs["filename"]) do
+        {:ok, new_attrs} ->
+          do_create_shape(Enum.into(new_attrs, attrs))
+
+        {:error, e} ->
+          {:error, e}
+      end
+    else
+      do_create_shape(attrs)
+    end
+  end
+
+  defp do_create_shape(attrs) do
     %Shape{}
     |> Shape.changeset(attrs)
     |> Repo.insert()
+  end
+
+  defp upload_shape(%Plug.Upload{} = upload) do
+    prefix = Application.get_env(:arrow, :shape_storage_prefix)
+    bucket = Application.get_env(:arrow, :shape_storage_bucket)
+    enabled? = Application.get_env(:arrow, :shape_storage_enabled?)
+    prefix_env = Application.get_env(:arrow, :shape_storage_prefix_env)
+    request_fn = Application.get_env(:arrow, :shape_storage_request_fn)
+
+    if enabled? do
+      prefix_env_value = System.get_env(prefix_env)
+      filename = upload.filename
+
+      path =
+        if prefix_env_value,
+          do: "#{prefix}#{prefix_env_value}/#{filename}",
+          else: "#{prefix}#{filename}"
+
+      case do_upload_shape(upload.path, bucket, path, request_fn) do
+        error = {:error, _} -> error
+        {:ok, _} -> {:ok, %{"bucket" => bucket, "prefix" => prefix, "path" => path}}
+      end
+    else
+      {:ok, %{"bucket" => "disabled", "prefix" => "disabled", "path" => "disabled"}}
+    end
+  end
+
+  defp do_upload_shape(local_path, bucket, remote_path, request_fn) do
+    {request_module, request_func} = request_fn
+    # Check if file exists already:
+    check_request = ExAws.S3.list_objects_v2(bucket, prefix: remote_path)
+    {:ok, check} = apply(request_module, request_func, [check_request])
+
+    if length(check.body.contents) > 0 do
+      {:error, :already_exists}
+    else
+      upload_request =
+        local_path
+        |> ExAws.S3.Upload.stream_file()
+        |> ExAws.S3.upload(bucket, remote_path)
+
+      {:ok, _} = apply(request_module, request_func, [upload_request])
+    end
+  end
+
+  defp delete_shape_file(shape) do
+    enabled? = Application.get_env(:arrow, :shape_storage_enabled?)
+
+    if enabled? do
+      ExAws.S3.delete_object(shape.bucket, shape.path) |> ExAws.request()
+    else
+      {:ok, :disabled}
+    end
   end
 
   @doc """
@@ -68,6 +136,22 @@ defmodule Arrow.Shuttle do
 
   """
   def update_shape(%Shape{} = shape, attrs) do
+    if Map.has_key?(attrs, "filename") do
+      case upload_shape(attrs["filename"]) do
+        {:ok, new_attrs} ->
+          # Delete old file:
+          {:ok, _} = delete_shape_file(shape)
+          do_shape_update(shape, Enum.into(new_attrs, attrs))
+
+        {:error, e} ->
+          {:error, e}
+      end
+    else
+      do_shape_update(shape, attrs)
+    end
+  end
+
+  defp do_shape_update(shape, attrs) do
     shape
     |> Shape.changeset(attrs)
     |> Repo.update()
@@ -86,6 +170,7 @@ defmodule Arrow.Shuttle do
 
   """
   def delete_shape(%Shape{} = shape) do
+    {:ok, _} = delete_shape_file(shape)
     Repo.delete(shape)
   end
 
--- test/arrow_web/controllers/shape_controller_test.exs
+++ test/arrow_web/controllers/shape_controller_test.exs
@@ -3,6 +3,13 @@ defmodule ArrowWeb.ShapeControllerTest do
 
   import Arrow.ShuttleFixtures
 
+  @create_s3_attrs %{
+    "name" => "some name",
+    "path" => "some/path/to/sample.kml",
+    "prefix" => "",
+    "bucket" => "",
+    "filename" => %Plug.Upload{filename: "sample.kml", path: "test_files/sample.kml"}
+  }
   @create_attrs %{name: "some name", filename: %Plug.Upload{filename: "some filename"}}
   @update_attrs %{name: "some updated name", filename: %Plug.Upload{filename: "some filename"}}
   @invalid_attrs %{name: nil}
@@ -20,6 +27,36 @@ defmodule ArrowWeb.ShapeControllerTest do
     test "renders form", %{conn: conn} do
       conn = get(conn, ~p"/shapes/new")
       assert html_response(conn, 200) =~ "New Shape"
+    end
+  end
+
+  describe "download shape" do
+    @tag :authenticated_admin
+    test "redirects to s3 url", %{conn: conn} do
+      # Allow storage, create safe prefix:
+      Application.put_env(:arrow, :shape_storage_enabled?, true)
+      uuid = Ecto.UUID.generate()
+      prefix = "arrow/test-runner/#{uuid}/"
+      Application.put_env(:arrow, :shape_storage_prefix, prefix)
+      bucket = Application.get_env(:arrow, :shape_storage_bucket)
+
+      # Create valid shape:
+      conn =
+        post(conn, ~p"/shapes",
+          shape: %{@create_s3_attrs | "prefix" => prefix, "bucket" => bucket}
+        )
+
+      assert %{id: id} = redirected_params(conn)
+      assert redirected_to(conn) == ~p"/shapes/#{id}"
+
+      # Attempt to download:
+      conn = ArrowWeb.ConnCase.authenticated_admin()
+      conn = get(conn, ~p"/shapes/#{id}/download")
+
+      assert redirected_to(conn, 302) ==
+               "https://#{Application.get_env(:arrow, :shape_storage_bucket)}.s3.amazonaws.com/arrow/test-runner/#{uuid}/sample.kml"
+
+      Application.put_env(:arrow, :shape_storage_enabled?, false)
     end
   end
 
