13aa2b681b8347ae804a38ff162729de7b2bf5ec
meagharty <149533950+meagharty@users.noreply.github.com>
2024-07-03 13:08:10 -0400

    feat: add shape selection and naming (#989)
    * feat: add shape selection and naming
    
    * feat: Add ShapeUpload as an embedded schema for shape selection
    
    * migration: drop coordinates column from db
    
    * fix: fix compiler warning by using hidden_input, naming update for shapes_upload
    
    * lint: update .credo.exs to run in strict mode to match CI
    
    * lint: fix ordering of imports from credo failure
    
    * fix: reset shapes_upload after error and remove now unused form

--- lib/arrow_web/controllers/shape_controller.ex
+++ lib/arrow_web/controllers/shape_controller.ex
@@ -1,6 +1,7 @@
 defmodule ArrowWeb.ShapeController do
   require Logger
-  alias Arrow.Shuttle.ShapeUpload
+  alias Arrow.Shuttle.ShapesUpload
+  alias Ecto.Changeset
   use ArrowWeb, :controller
 
   alias Arrow.Shuttle
@@ -17,28 +18,73 @@ defmodule ArrowWeb.ShapeController do
   end
 
   def new(conn, %{}) do
-    changeset_map = ShapeUpload.changeset(%ShapeUpload{shapes: %{}}, %{})
-    render(conn, :new_bulk, shape_upload: changeset_map)
+    changeset_map = ShapesUpload.changeset(%ShapesUpload{shapes: []}, %{})
+    render(conn, :new_bulk, shapes_upload: changeset_map)
   end
 
-  def create(conn, %{"shape_upload" => shape_upload}) do
-    with {:ok, saxy_shapes} <- ShapeUpload.parse_kml_from_file(shape_upload),
-         {:ok, shapes} <- ShapeUpload.shapes_from_kml(saxy_shapes),
-         {:ok, changesets} <- Shuttle.create_shapes(shapes) do
+  def create(conn, %{"shapes_upload" => shapes_upload}) do
+    filename = shapes_upload["filename"].filename
+    reset_upload = ShapesUpload.changeset(%ShapesUpload{shapes: []}, %{})
+
+    with {:ok, saxy_shapes} <- ShapesUpload.parse_kml_from_file(shapes_upload),
+         {:ok, shapes} <- ShapesUpload.shapes_from_kml(saxy_shapes),
+         %Changeset{valid?: true} = changeset <-
+           ShapesUpload.changeset(%ShapesUpload{}, %{filename: filename, shapes: shapes}) do
       conn
       |> put_flash(
         :info,
-        "Uploaded successfully #{inspect(changesets)}"
+        "Successfully parsed shapes #{inspect(shapes)} from file"
       )
-      |> redirect(to: ~p"/shapes/")
+      |> render(:select, form: changeset |> Phoenix.Component.to_form())
     else
+      {:error, reason} ->
+        conn
+        |> put_flash(:errors, reason)
+        |> render(:new_bulk, errors: reason, shapes_upload: reset_upload)
+
+      error ->
+        conn
+        |> put_flash(:errors, error)
+        |> render(:new_bulk, errors: error, shapes_upload: reset_upload)
+    end
+  end
+
+  def create(conn, %{"shapes" => shapes}) do
+    reset_upload = ShapesUpload.changeset(%ShapesUpload{shapes: []}, %{})
+
+    saved_shapes =
+      shapes
+      |> Enum.map(fn {_idx, shape} -> shape end)
+      |> Enum.filter(fn shape -> shape["save"] == "true" end)
+
+    case Shuttle.create_shapes(saved_shapes) do
+      {:ok, []} ->
+        conn
+        |> put_flash(
+          :info,
+          "No shapes were marked to be saved"
+        )
+        |> redirect(to: ~p"/shapes/")
+
+      {:ok, changesets} ->
+        saved_shape_names =
+          changesets
+          |> Enum.map(fn {:ok, changeset} -> changeset.name end)
+
+        conn
+        |> put_flash(
+          :info,
+          "Successfully saved shapes: #{inspect(saved_shape_names)}"
+        )
+        |> redirect(to: ~p"/shapes/")
+
       {:error, reason} ->
         conn
         |> put_flash(
           :errors,
           reason
         )
-        |> render(:new_bulk, shape_upload: shape_upload, errors: reason)
+        |> render(:new_bulk, shapes_upload: reset_upload, errors: reason)
     end
   end
 
