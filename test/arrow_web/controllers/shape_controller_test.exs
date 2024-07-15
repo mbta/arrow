2244d25ebea46468e254e18324223030fa4c095a
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-07-15 14:32:49 -0400

    tests: more tests, create a separate block for upload and create shape

--- test/arrow_web/controllers/shape_controller_test.exs
+++ test/arrow_web/controllers/shape_controller_test.exs
@@ -12,11 +12,25 @@ defmodule ArrowWeb.ShapeControllerTest do
       filename: "some filename"
     }
   }
+  @bulk_upload_attrs %{
+    name: nil,
+    filename: %Plug.Upload{
+      path: "test/support/fixtures/kml/multiple_shapes.kml",
+      filename: "multiple_shapes.kml"
+    }
+  }
   @invalid_upload_attrs %{
     name: nil,
     filename: %Plug.Upload{
       path: "test/support/fixtures/kml/invalid_file.kml",
       filename: "invalid_file.kml"
+    }
+  }
+  @invalid_whitespace_attrs %{
+    name: nil,
+    filename: %Plug.Upload{
+      path: "test/support/fixtures/kml/invalid_whitespace_shape.kml",
+      filename: "invalid_whitespace_shape.kml"
     }
   }
   @file_read_fail_attrs %{
@@ -76,9 +90,9 @@ defmodule ArrowWeb.ShapeControllerTest do
     end
   end
 
-  describe "create shape" do
+  describe "shapes_upload for create" do
     @tag :authenticated_admin
-    test "redirects to select when upload file is valid", %{conn: conn} do
+    test "redirects to select when upload file is valid with one shape", %{conn: conn} do
       conn = post(conn, ~p"/shapes_upload", shapes_upload: @upload_attrs)
       assert html_response(conn, 200) =~ "Successfully parsed shapes"
       assert html_response(conn, 200) =~ "RL: Alewife - Harvard - Via Brattle St - Harvard"
@@ -86,15 +100,11 @@ defmodule ArrowWeb.ShapeControllerTest do
     end
 
     @tag :authenticated_admin
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, ~p"/shapes_upload", shapes: @create_attrs)
-
-      assert redirected_to(conn) == ~p"/shapes/"
-
-      conn = ArrowWeb.ConnCase.authenticated_admin()
-      conn = get(conn, ~p"/shapes")
-      assert html_response(conn, 200) =~ "some name"
-      refute html_response(conn, 200) =~ "some other name"
+    test "redirects to select when upload file is valid with many shapes", %{conn: conn} do
+      conn = post(conn, ~p"/shapes_upload", shapes_upload: @bulk_upload_attrs)
+      assert html_response(conn, 200) =~ "Successfully parsed shapes"
+      assert html_response(conn, 200) =~ "RL: JFK/UMass - Andrew - JFK/UMASS"
+      assert html_response(conn, 200) =~ "Shapes from File"
     end
 
     @tag :authenticated_admin
@@ -107,11 +117,36 @@ defmodule ArrowWeb.ShapeControllerTest do
     end
 
     @tag :authenticated_admin
+    test "renders errors when data is invalid due to invalid whitespace in XML", %{conn: conn} do
+      conn = post(conn, ~p"/shapes_upload", shapes_upload: @invalid_whitespace_attrs)
+
+      assert html_response(conn, 200) =~
+               "Failed to parse shape from kml, no coordinates were found."
+
+      assert html_response(conn, 200) =~ "Check your whitespace."
+      assert html_response(conn, 200) =~ "New Shapes"
+    end
+
+    @tag :authenticated_admin
     test "renders errors when file read fails", %{conn: conn} do
       conn = post(conn, ~p"/shapes_upload", shapes_upload: @file_read_fail_attrs)
       assert html_response(conn, 200) =~ "Failed to upload shapes from some_file.kml"
       assert html_response(conn, 200) =~ "no such file or directory"
       assert html_response(conn, 200) =~ "New Shapes"
+    end
+  end
+
+  describe "create shapes" do
+    @tag :authenticated_admin
+    test "redirects to show when data is valid", %{conn: conn} do
+      conn = post(conn, ~p"/shapes_upload", shapes: @create_attrs)
+
+      assert redirected_to(conn) == ~p"/shapes/"
+
+      conn = ArrowWeb.ConnCase.authenticated_admin()
+      conn = get(conn, ~p"/shapes")
+      assert html_response(conn, 200) =~ "some name"
+      refute html_response(conn, 200) =~ "some other name"
     end
   end
 
