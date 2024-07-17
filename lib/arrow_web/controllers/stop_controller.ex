6cbf201289d154a624135a12d121ee0528af460d
Walton Hoops <me@waltonhoops.com>
2024-07-17 13:20:57 -0600

    chore: autogenerate view/controller

--- lib/arrow_web/controllers/stop_controller.ex
+++ lib/arrow_web/controllers/stop_controller.ex
@@ -1,30 +1,54 @@
 defmodule ArrowWeb.StopController do
   use ArrowWeb, :controller
 
+  alias Arrow.Shuttle.Stop
+  alias Arrow.Stops
   alias Plug.Conn
 
   @spec index(Conn.t(), Conn.params()) :: Conn.t()
   def index(conn, _params) do
-    render(conn, "index.html", [])
+    stops = Stops.list_stops()
+    render(conn, :index, stops: stops)
   end
 
   @spec new(Conn.t(), Conn.params()) :: Conn.t()
   def new(conn, _params) do
-    render(conn, "new.html", [])
+    changeset = Stops.change_stop(%Stop{})
+    render(conn, :new, changeset: changeset)
   end
 
   @spec create(Conn.t(), Conn.params()) :: Conn.t()
-  def create(conn, _params) do
-    render(conn, "new.html", [])
+  def create(conn, %{"stop" => stop_params}) do
+    case Stops.create_stop(stop_params) do
+      {:ok, _stop} ->
+        conn
+        |> put_flash(:info, "Stop created successfully.")
+        |> redirect(to: ~p"/stops")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
   end
 
   @spec edit(Conn.t(), Conn.params()) :: Conn.t()
-  def edit(conn, _parmas) do
-    render(conn, "edit.html", [])
+  def edit(conn, %{"id" => id}) do
+    stop = Stops.get_stop!(id)
+    changeset = Stops.change_stop(stop)
+    render(conn, :edit, stop: stop, changeset: changeset)
   end
 
   @spec update(Conn.t(), Conn.params()) :: Conn.t()
-  def update(conn, %{"id" => _id} = _params) do
-    render(conn, "edit.html", [])
+  def update(conn, %{"id" => id, "stop" => stop_params}) do
+    stop = Stops.get_stop!(id)
+
+    case Stops.update_stop(stop, stop_params) do
+      {:ok, _stop} ->
+        conn
+        |> put_flash(:info, "Stop updated successfully.")
+        |> redirect(to: ~p"/stops")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :edit, stop: stop, changeset: changeset)
+    end
   end
 end
--- lib/arrow_web/controllers/stop_html.ex
+++ lib/arrow_web/controllers/stop_html.ex
@@ -2,4 +2,12 @@ defmodule ArrowWeb.StopView do
   use ArrowWeb, :html
 
   embed_templates "stop_html/*"
+
+  @doc """
+  Renders a stop form.
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+
+  def stop_form(assigns)
 end
