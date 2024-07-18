bf099fd237df8da4ea23f73d3956f03138cc5dcf
Brian Fauble <bfauble@users.noreply.github.com>
2024-07-18 08:12:10 -0600

    add initial shape visualization

--- assets/src/components/ShapeViewMap.tsx
+++ assets/src/components/ShapeViewMap.tsx
@@ -1,18 +1,29 @@
-import React from "react"
+import React, { useMemo } from "react"
 import { LatLngExpression } from "leaflet"
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, Polyline, TileLayer } from "react-leaflet"
+import { useMap } from 'react-leaflet/hooks'
+
 
 const defaultCenter: LatLngExpression = [42.360718, -71.05891]
 
-const ShapeViewMap = () => {
+const generatePolyline = (coordinates) => <Polyline positions={coordinates} />;
+
+const getMapBounds = (shapes) => {
+  let shapeCoords = []
+  return shapes.forEach(shape => shapeCoords + shape.coordinates)
+}
+
+const ShapeViewMap = ({ shapes }) => {
+  const polyLines = useMemo(() => shapes.map(shape => generatePolyline(shape.coordinates)), shapes);
   return (
     <MapContainer
       data-testid="shape-view-map-container"
       style={{ height: "800px" }}
-      center={defaultCenter}
+      bounds={getMapBounds(shapes)}
       zoom={13}
       scrollWheelZoom={true}
     >
+      {polyLines}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- lib/arrow_web/controllers/shape_html.ex
+++ lib/arrow_web/controllers/shape_html.ex
@@ -1,5 +1,7 @@
 defmodule ArrowWeb.ShapeView do
   use ArrowWeb, :html
+  alias Arrow.Shuttle.ShapesUpload
+  require Logger
 
   embed_templates "shape_html/*"
 
@@ -10,4 +12,29 @@ defmodule ArrowWeb.ShapeView do
   attr :action, :string, required: true
 
   def shape_form(assigns)
+
+  def shapes_map_view(%ShapesUpload{shapes: shapes}) do
+    %{shapes: Enum.map(shapes, &shape_map_view/1)}
+  end
+  def shapes_map_view(%{params: %{"shapes" => shapes}}) do
+    %{shapes: Enum.map(shapes, &shape_map_view/1)}
+  end
+
+  defp shape_map_view(%{coordinates: coordinates, name: name}) do
+    %{
+      coordinates: map_coordinates(coordinates),
+      name: name
+    }
+  end
+
+  defp map_coordinates(coordinates) do
+    Enum.map(coordinates, &process_coordinate_pair/1)
+  end
+
+  defp process_coordinate_pair(coordinate_pair) do
+    coordinate_pair
+    |> String.split(",")
+    |> Enum.map(&String.to_float/1)
+    |> Enum.reverse()
+  end
 end
