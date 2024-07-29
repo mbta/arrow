80481c857328ea30714cad24302df640734f5214
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-07-29 16:24:02 -0400

    feat: add stop marker on stop edit page

--- assets/src/components/StopViewMap.tsx
+++ assets/src/components/StopViewMap.tsx
@@ -1,10 +1,23 @@
 import React from "react"
-import { LatLngExpression } from "leaflet"
-import { MapContainer, TileLayer } from "react-leaflet"
+import { LatLngExpression, icon } from "leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+
+type Stop = {
+  name: string
+  description: string
+  latitude: number
+  longitude: number
+}
 
 const defaultCenter: LatLngExpression = [42.360718, -71.05891]
 
-const StopViewMap = () => {
+const markerIcon = icon({
+  iconUrl: "/images/marker-icon.png",
+  iconRetinaUrl: "/images/marker-icon-2x.png",
+  shadowUrl: "/images/marker-shadow.png",
+})
+
+const StopViewMap = ({ stop }: { stop?: Stop }) => {
   return (
     <MapContainer
       data-testid="stop-view-map-container"
@@ -17,6 +30,11 @@ const StopViewMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {stop && (
+        <Marker position={[stop.latitude, stop.longitude]} icon={markerIcon}>
+          <Popup>{stop.name}</Popup>
+        </Marker>
+      )}
     </MapContainer>
   )
 }
