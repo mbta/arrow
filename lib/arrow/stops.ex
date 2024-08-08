0ebc0689ed2cd47631f4d7948f81e9807b68a9d7
Brian Fauble <bfauble@users.noreply.github.com>
2024-08-08 10:59:43 -0500

    🏹 Implement sort functionality within "View Shuttle Stops" page (#997)
    * add sorting functionality to stops page table
    
    * format and add sort test
    
    * adjust icon size
    
    * credo

--- lib/arrow/stops.ex
+++ lib/arrow/stops.ex
@@ -17,8 +17,12 @@ defmodule Arrow.Stops do
       [%Stop{}, ...]
 
   """
-  def list_stops do
-    Repo.all(Stop)
+  def list_stops(params \\ %{}) do
+    from(
+      s in Stop,
+      order_by: ^order_by(params["order_by"])
+    )
+    |> Repo.all()
   end
 
   @doc """
@@ -101,4 +105,36 @@ defmodule Arrow.Stops do
   def change_stop(%Stop{} = stop, attrs \\ %{}) do
     Stop.changeset(stop, attrs)
   end
+
+  defp order_by("stop_id_desc"), do: [desc: :stop_id]
+  defp order_by("stop_id_asc"), do: [asc: :stop_id]
+  defp order_by("stop_name_desc"), do: [desc: :stop_name]
+  defp order_by("stop_name_asc"), do: [asc: :stop_name]
+  defp order_by("stop_desc_desc"), do: [desc: :stop_desc]
+  defp order_by("stop_desc_asc"), do: [asc: :stop_desc]
+  defp order_by("platform_code_desc"), do: [desc: :platform_code]
+  defp order_by("platform_code_asc"), do: [asc: :platform_code]
+  defp order_by("platform_name_desc"), do: [desc: :platform_name]
+  defp order_by("platform_name_asc"), do: [asc: :platform_name]
+  defp order_by("stop_lat_desc"), do: [desc: :stop_lat]
+  defp order_by("stop_lat_asc"), do: [asc: :stop_lat]
+  defp order_by("stop_lon_desc"), do: [desc: :stop_lon]
+  defp order_by("stop_lon_asc"), do: [asc: :stop_lon]
+  defp order_by("stop_address_desc"), do: [desc: :stop_address]
+  defp order_by("stop_address_asc"), do: [asc: :stop_address]
+  defp order_by("zone_id_desc"), do: [desc: :zone_id]
+  defp order_by("zone_id_asc"), do: [asc: :zone_id]
+  defp order_by("level_id_desc"), do: [desc: :level_id]
+  defp order_by("level_id_asc"), do: [asc: :level_id]
+  defp order_by("parent_station_desc"), do: [desc: :parent_station]
+  defp order_by("parent_station_asc"), do: [asc: :parent_station]
+  defp order_by("municipality_desc"), do: [desc: :municipality]
+  defp order_by("municipality_asc"), do: [asc: :municipality]
+  defp order_by("on_street_desc"), do: [desc: :on_street]
+  defp order_by("on_street_asc"), do: [asc: :on_street]
+  defp order_by("at_street_desc"), do: [desc: :at_street]
+  defp order_by("at_street_asc"), do: [asc: :at_street]
+  defp order_by("updated_at_desc"), do: [desc: :updated_at]
+  defp order_by("updated_at_asc"), do: [asc: :updated_at]
+  defp order_by(_), do: []
 end
