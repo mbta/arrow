e56dd1613bcf3c8722bcf277a33dfa9d7ea909cc
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-10-04 17:55:05 -0400

    fix-up rename of shuttles

--- lib/arrow/shuttles.ex
+++ lib/arrow/shuttles.ex
@@ -3,8 +3,234 @@ defmodule Arrow.Shuttles do
   The Shuttles context.
   """
 
-  import Ecto.Query, warn: false
-  alias Arrow.Repo
+import Ecto.Query, warn: false
+
+alias Arrow.Repo
+alias ArrowWeb.ErrorHelpers
+
+alias Arrow.Shuttles.KML
+alias Arrow.Shuttles.Shape
+alias Arrow.Shuttles.ShapesUpload
+alias Arrow.Shuttles.ShapeUpload
+
+@doc """
+Returns the list of shapes.
+
+## Examples
+
+    iex> list_shapes()
+    [%Shape{}, ...]
+
+"""
+def list_shapes do
+  Repo.all(Shape)
+end
+
+@doc """
+Gets a single shape.
+
+Raises `Ecto.NoResultsError` if the Shape does not exist.
+
+## Examples
+
+    iex> get_shape!(123)
+    %Shape{}
+
+    iex> get_shape!(456)
+    ** (Ecto.NoResultsError)
+
+"""
+def get_shape!(id), do: Repo.get!(Shape, id)
+
+@doc """
+Gets a shapes upload struct associated with a given shape.
+
+## Examples
+
+    iex> get_shapes_upload(%Shape{})
+    %ShapesUpload{}
+"""
+def get_shapes_upload(%Shape{} = shape) do
+  with true <- Application.get_env(:arrow, :shape_storage_enabled?),
+       {:ok, %{body: shapes_kml}} <- get_shape_file(shape),
+       {:ok, parsed_shapes} <- ShapesUpload.parse_kml(shapes_kml),
+       {:ok, shapes} <- ShapesUpload.shapes_from_kml(parsed_shapes) do
+    ShapesUpload.changeset(%ShapesUpload{}, %{filename: shape.name, shapes: shapes})
+  else
+    false -> {:ok, :disabled}
+    error -> error
+  end
+end
+
+defp get_shape_file(shape) do
+  enabled? = Application.get_env(:arrow, :shape_storage_enabled?)
+  {request_module, request_func} = Application.get_env(:arrow, :shape_storage_request_fn)
+
+  if enabled? do
+    get_request = ExAws.S3.get_object(shape.bucket, shape.path)
+    apply(request_module, request_func, [get_request])
+  else
+    {:ok, :disabled}
+  end
+end
+
+@spec create_shapes(any()) :: {:error, {<<_::224>>, list()}} | {:ok, list()}
+@doc """
+Creates shapes.
+
+"""
+def create_shapes(shapes) do
+  changesets = Enum.map(shapes, fn shape -> create_shape(shape) end)
+
+  case Enum.all?(changesets, fn changeset -> Kernel.match?({:ok, _shape}, changeset) end) do
+    true ->
+      {:ok, changesets}
+
+    _ ->
+      errors =
+        changesets
+        |> Enum.filter(fn changeset -> Kernel.match?({:error, _}, changeset) end)
+        |> Enum.map(&handle_create_error/1)
+
+      {:error, {"Failed to upload some shapes", errors}}
+  end
+end
+
+def handle_create_error({_, message}) when is_binary(message) do
+  message
+end
+
+def handle_create_error({_, %Ecto.Changeset{} = changeset}) do
+  "#{ErrorHelpers.changeset_error_messages(changeset)} #{changeset.params["name"]}"
+end
+
+@doc """
+Creates a shape.
+
+## Examples
+
+    iex> create_shape(%{field: value})
+    {:ok, %Shape{}}
+
+    iex> create_shape(%{field: bad_value})
+    {:error, %Ecto.Changeset{}}
+
+"""
+def create_shape(attrs) do
+  with {:ok, attrs} <- Shape.validate_and_enforce_name(attrs),
+       nil <- Repo.get_by(Shape, name: attrs.name),
+       {:ok, shape_with_kml} <- create_shape_kml(attrs),
+       {:ok, new_attrs} <- upload_shape_file(shape_with_kml) do
+    do_create_shape(Enum.into(new_attrs, attrs))
+  else
+    %Shape{name: name} ->
+      {:error, "Shape #{name} already exists, delete the shape to save a new one"}
+
+    {:error, :already_exists} ->
+      {:error,
+       "File for shape #{attrs.name} already exists, delete the shape to save a new one"}
+
+    {:error, e} ->
+      {:error, e}
+  end
+end
+
+defp do_create_shape(attrs) do
+  %Shape{}
+  |> Shape.changeset(attrs)
+  |> Repo.insert()
+end
+
+def create_shape_kml(%{name: _name, coordinates: _coordinates} = attrs) do
+  kml = %KML{xmlns: "http://www.opengis.net/kml/2.2", Folder: attrs}
+  shape_kml = Saxy.Builder.build(kml)
+  content = Saxy.encode!(shape_kml, version: "1.0", encoding: "UTF-8")
+  {:ok, Enum.into(%{content: content}, attrs)}
+end
+
+def create_shape_kml(attrs) do
+  {:error, ShapeUpload.changeset(%ShapeUpload{}, attrs)}
+end
+
+defp upload_shape_file(%{name: name, content: content}) do
+  prefix = Application.get_env(:arrow, :shape_storage_prefix)
+  bucket = Application.get_env(:arrow, :shape_storage_bucket)
+  enabled? = Application.get_env(:arrow, :shape_storage_enabled?)
+  prefix_env = Application.get_env(:arrow, :shape_storage_prefix_env)
+  request_fn = Application.get_env(:arrow, :shape_storage_request_fn)
+
+  if enabled? and prefix_env != nil do
+    filename = "#{name}.kml"
+
+    path =
+      if prefix_env,
+        do: "#{prefix_env}#{prefix}#{filename}",
+        else: "#{prefix}#{filename}"
+
+    case do_upload_shape(content, bucket, path, request_fn) do
+      error = {:error, _} -> error
+      {:ok, _} -> {:ok, %{bucket: bucket, prefix: prefix, path: path}}
+    end
+  else
+    {:ok, %{bucket: "disabled", prefix: "disabled", path: "disabled"}}
+  end
+end
+
+defp do_upload_shape(content, bucket, remote_path, request_fn) do
+  {request_module, request_func} = request_fn
+  # Check if file exists already:
+  check_request = ExAws.S3.list_objects_v2(bucket, prefix: remote_path)
+  {:ok, check} = apply(request_module, request_func, [check_request])
+
+  if length(check.body.contents) > 0 do
+    {:error, :already_exists}
+  else
+    upload_request = ExAws.S3.put_object(bucket, remote_path, content)
+    {:ok, _} = apply(request_module, request_func, [upload_request])
+  end
+end
+
+defp delete_shape_file(shape) do
+  enabled? = Application.get_env(:arrow, :shape_storage_enabled?)
+  {request_module, request_func} = Application.get_env(:arrow, :shape_storage_request_fn)
+
+  if enabled? do
+    delete_request = ExAws.S3.delete_object(shape.bucket, shape.path)
+    apply(request_module, request_func, [delete_request])
+  else
+    {:ok, :disabled}
+  end
+end
+
+@doc """
+Deletes a shape.
+
+## Examples
+
+    iex> delete_shape(shape)
+    {:ok, %Shape{}}
+
+    iex> delete_shape(shape)
+    {:error, %Ecto.Changeset{}}
+
+"""
+def delete_shape(%Shape{} = shape) do
+  {:ok, _} = delete_shape_file(shape)
+  Repo.delete(shape)
+end
+
+@doc """
+Returns an `%Ecto.Changeset{}` for tracking shape changes.
+
+## Examples
+
+    iex> change_shape(shape)
+    %Ecto.Changeset{data: %Shape{}}
+
+"""
+def change_shape(%Shape{} = shape, attrs \\ %{}) do
+  Shape.changeset(shape, attrs)
+end
 
   alias Arrow.Shuttles.Shuttle
 
@@ -196,5 +422,101 @@ defmodule Arrow.Shuttles do
   """
   def change_shuttle_route(%ShuttleRoute{} = shuttle_route, attrs \\ %{}) do
     ShuttleRoute.changeset(shuttle_route, attrs)
+  end
+
+  alias Arrow.Shuttles.ShuttleRouteStops
+
+  @doc """
+  Returns the list of shuttle_route_stops.
+
+  ## Examples
+
+      iex> list_shuttle_route_stops()
+      [%ShuttleRouteStops{}, ...]
+
+  """
+  def list_shuttle_route_stops do
+    Repo.all(ShuttleRouteStops)
+  end
+
+  @doc """
+  Gets a single shuttle_route_stops.
+
+  Raises `Ecto.NoResultsError` if the Shuttle route stops does not exist.
+
+  ## Examples
+
+      iex> get_shuttle_route_stops!(123)
+      %ShuttleRouteStops{}
+
+      iex> get_shuttle_route_stops!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_shuttle_route_stops!(id), do: Repo.get!(ShuttleRouteStops, id)
+
+  @doc """
+  Creates a shuttle_route_stops.
+
+  ## Examples
+
+      iex> create_shuttle_route_stops(%{field: value})
+      {:ok, %ShuttleRouteStops{}}
+
+      iex> create_shuttle_route_stops(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_shuttle_route_stops(attrs \\ %{}) do
+    %ShuttleRouteStops{}
+    |> ShuttleRouteStops.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a shuttle_route_stops.
+
+  ## Examples
+
+      iex> update_shuttle_route_stops(shuttle_route_stops, %{field: new_value})
+      {:ok, %ShuttleRouteStops{}}
+
+      iex> update_shuttle_route_stops(shuttle_route_stops, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_shuttle_route_stops(%ShuttleRouteStops{} = shuttle_route_stops, attrs) do
+    shuttle_route_stops
+    |> ShuttleRouteStops.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a shuttle_route_stops.
+
+  ## Examples
+
+      iex> delete_shuttle_route_stops(shuttle_route_stops)
+      {:ok, %ShuttleRouteStops{}}
+
+      iex> delete_shuttle_route_stops(shuttle_route_stops)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_shuttle_route_stops(%ShuttleRouteStops{} = shuttle_route_stops) do
+    Repo.delete(shuttle_route_stops)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking shuttle_route_stops changes.
+
+  ## Examples
+
+      iex> change_shuttle_route_stops(shuttle_route_stops)
+      %Ecto.Changeset{data: %ShuttleRouteStops{}}
+
+  """
+  def change_shuttle_route_stops(%ShuttleRouteStops{} = shuttle_route_stops, attrs \\ %{}) do
+    ShuttleRouteStops.changeset(shuttle_route_stops, attrs)
   end
 end
