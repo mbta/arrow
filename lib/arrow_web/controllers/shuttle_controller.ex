7203404b0ed703daf8e2aaa2552cf4fff820b294
Meag Harty <149533950+meagharty@users.noreply.github.com>
2024-10-11 15:37:24 -0400

    feat: add additional scaffolding for shuttle page

--- lib/arrow_web/controllers/shuttle_controller.ex
+++ lib/arrow_web/controllers/shuttle_controller.ex
@@ -1,28 +1,53 @@
 defmodule ArrowWeb.ShuttleController do
   use ArrowWeb, :controller
+
+  alias Arrow.Shuttles
   alias Arrow.Shuttles.Shuttle
 
   def index(conn, _params) do
-    render(conn, :index, shuttles: [])
+    shuttles = Shuttles.list_shuttles()
+    render(conn, :index, shuttles: shuttles)
   end
 
   def new(conn, _params) do
-    render(conn, :new, changeset: nil)
+    changeset = Shuttles.change_shuttle(%Shuttle{})
+    render(conn, :new, changeset: changeset)
   end
 
-  def create(conn, %{"shuttle" => _shuttle_params}) do
-    render(conn, :new, changeset: nil)
+  def create(conn, %{"shuttle" => shuttle_params}) do
+    case Shuttles.create_shuttle(shuttle_params) do
+      {:ok, shuttle} ->
+        conn
+        |> put_flash(:info, "Shuttle created successfully.")
+        |> redirect(to: ~p"/shuttles/#{shuttle}")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
   end
 
-  def show(conn, %{"id" => _id}) do
-    render(conn, :show, shuttle: nil)
+  def show(conn, %{"id" => id}) do
+    shuttle = Shuttles.get_shuttle!(id)
+    render(conn, :show, shuttle: shuttle)
   end
 
   def edit(conn, %{"id" => id}) do
-    render(conn, :edit, shuttle: %Shuttle{id: id}, changeset: nil)
+    shuttle = Shuttles.get_shuttle!(id)
+    changeset = Shuttles.change_shuttle(shuttle)
+    render(conn, :edit, shuttle: shuttle, changeset: changeset)
   end
 
-  def update(conn, %{"id" => _id, "shuttle" => _shuttle_params}) do
-    render(conn, :edit, shuttle: nil, changeset: nil)
+  def update(conn, %{"id" => id, "shuttle" => shuttle_params}) do
+    shuttle = Shuttles.get_shuttle!(id)
+
+    case Shuttles.update_shuttle(shuttle, shuttle_params) do
+      {:ok, shuttle} ->
+        conn
+        |> put_flash(:info, "Shuttle updated successfully.")
+        |> redirect(to: ~p"/shuttles/#{shuttle}")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :edit, shuttle: shuttle, changeset: changeset)
+    end
   end
 end
