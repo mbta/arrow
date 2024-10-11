62a89fc09db691f7a572c35a0f5448d5f5b54e37
meagharty <149533950+meagharty@users.noreply.github.com>
2024-10-11 12:39:49 -0400

    feat: add schemas for shuttles (#1017)
    * feat: add schema for shuttle, with gtfs_routes dependency commented out
    
    * feat: add shuttle_routes schema
    
    * feat: add shuttle_route_stops schema
    
    * fix: add seeds for local development
    
    * fix: move files with renamed Shuttles context to new shuttles dir
    
    * fix: fix default timestamp to be timestamp with timezone
    
    * chore: run migrations to update database schema

---
+++ lib/arrow/shuttles/route.ex
@@ -0,0 +1,24 @@
+defmodule Arrow.Shuttles.Route do
+  @moduledoc "schema for a shuttle route for the db"
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "shuttle_routes" do
+    field :suffix, :string
+    field :destination, :string
+    field :direction_id, Ecto.Enum, values: [:"0", :"1"]
+    field :direction_desc, :string
+    field :waypoint, :string
+    field :shuttle_id, :id
+    field :shape_id, :id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(route, attrs) do
+    route
+    |> cast(attrs, [:direction_id, :direction_desc, :destination, :waypoint, :suffix])
+    |> validate_required([:direction_id, :direction_desc, :destination])
+  end
+end
