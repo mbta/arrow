95da1f7aafa744767a638927a3bf86dd5d067340
Eddie Maldonado <emaldonado@mbta.com>
2024-11-06 14:20:30 -0500

    refactor: remove unneeded on_replace: :delete

--- lib/arrow/shuttles/shuttle.ex
+++ lib/arrow/shuttles/shuttle.ex
@@ -8,9 +8,7 @@ defmodule Arrow.Shuttles.Shuttle do
     field :shuttle_name, :string
     field :disrupted_route_id, :string
 
-    has_many :routes, Arrow.Shuttles.Route,
-      preload_order: [asc: :direction_id],
-      on_replace: :delete
+    has_many :routes, Arrow.Shuttles.Route, preload_order: [asc: :direction_id]
 
     timestamps(type: :utc_datetime)
   end
