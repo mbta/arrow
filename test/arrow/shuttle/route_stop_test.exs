f55d8ceb8801da35a7fc1d8a877ad5a90da47269
Eddie Maldonado <emaldonado@mbta.com>
2024-12-04 09:39:21 -0500

    test: tests for RouteStop.changeset/2

---
+++ test/arrow/shuttle/route_stop_test.exs
@@ -0,0 +1,64 @@
+defmodule Arrow.Shuttles.RouteStopTest do
+  use Arrow.DataCase
+
+  alias Arrow.Shuttles.RouteStop
+
+  import Arrow.Factory
+
+  describe "changeset/2" do
+    test "handles GTFS stop" do
+      gtfs_stop = insert(:gtfs_stop)
+
+      gtfs_stop_id = gtfs_stop.id
+
+      route_stop = insert(:route_stop)
+
+      changeset = RouteStop.changeset(route_stop, %{"display_stop_id" => gtfs_stop_id})
+
+      assert %Ecto.Changeset{
+               valid?: true,
+               changes: %{display_stop_id: ^gtfs_stop_id, gtfs_stop_id: ^gtfs_stop_id}
+             } = changeset
+    end
+
+    test "handles Arrow stop" do
+      stop = insert(:stop)
+
+      display_stop_id = stop.stop_id
+      stop_id = stop.id
+
+      route_stop = insert(:route_stop)
+
+      changeset = RouteStop.changeset(route_stop, %{"display_stop_id" => display_stop_id})
+
+      assert %Ecto.Changeset{
+               valid?: true,
+               changes: %{display_stop_id: ^display_stop_id, stop_id: ^stop_id}
+             } = changeset
+    end
+
+    test "gives an error when stop ID is invalid" do
+      route_stop = insert(:route_stop)
+
+      changeset = RouteStop.changeset(route_stop, %{"display_stop_id" => "invalid_id"})
+
+      assert %Ecto.Changeset{
+               valid?: false,
+               errors: [display_stop_id: {"not a valid stop ID", []}]
+             } = changeset
+    end
+
+    test "can update other fields" do
+      route_stop = insert(:route_stop)
+
+      changeset = RouteStop.changeset(route_stop, %{"time_to_next_stop" => 10})
+
+      time_to_next_stop_change_value = Decimal.new("10")
+
+      assert %Ecto.Changeset{
+               valid?: true,
+               changes: %{time_to_next_stop: ^time_to_next_stop_change_value}
+             } = changeset
+    end
+  end
+end
