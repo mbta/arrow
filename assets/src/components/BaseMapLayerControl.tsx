a3b5927c2a3601d0f5da6ebda98118edf933632d
JD <jd@rudiejd.io>
2024-12-09 16:26:26 -0500

    fixup: attribution

--- assets/src/components/BaseMapLayerControl.tsx
+++ assets/src/components/BaseMapLayerControl.tsx
@@ -10,13 +10,13 @@ const BaseMapLayerControl = () => {
     >
       <LayersControl.BaseLayer name="Map" checked={true}>
         <TileLayer
-          attribution='&copy; <a href="https://www.mass.gov/info-details/massgis-data-2021-aerial-imagery">MassGIS 2021</a>'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://mbta-map-tiles.s3.amazonaws.com/skate_osm_tiles/{z}/{x}/{y}.png"
         />
       </LayersControl.BaseLayer>
       <LayersControl.BaseLayer name="Satellite">
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.mass.gov/info-details/massgis-data-2021-aerial-imagery">MassGIS 2021</a>'
           url="https://tiles.arcgis.com/tiles/hGdibHYSPO59RG1h/arcgis/rest/services/orthos2023/MapServer/tile/{z}/{y}/{x}"
         />
       </LayersControl.BaseLayer>
