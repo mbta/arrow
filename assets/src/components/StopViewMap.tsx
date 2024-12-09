7d076eb775769be8a5df56efb945f6029379771c
JD <jd@rudiejd.io>
2024-12-09 15:26:36 -0500

    fixup: use layer component on stop view map

--- assets/src/components/StopViewMap.tsx
+++ assets/src/components/StopViewMap.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { LatLngExpression, icon } from "leaflet"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import BaseMapLayerControl from "./BaseMapLayerControl"
 
 type Stop = {
   stop_name: string
@@ -11,14 +12,6 @@ type Stop = {
 
 const defaultCenter: LatLngExpression = [42.360718, -71.05891]
 
-const markerIcon = icon({
-  iconUrl: "/images/marker-icon.png",
-  iconRetinaUrl: "/images/marker-icon-2x.png",
-  shadowUrl: "/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-})
-
 const StopViewMap = ({ stop }: { stop?: Stop }) => {
   return (
     <MapContainer
@@ -28,10 +21,7 @@ const StopViewMap = ({ stop }: { stop?: Stop }) => {
       zoom={13}
       scrollWheelZoom={true}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <BaseMapLayerControl />
       {stop && stop.stop_lat && stop.stop_lon && (
         <Marker position={[stop.stop_lat, stop.stop_lon]} icon={markerIcon}>
           <Popup>{stop.stop_name}</Popup>
