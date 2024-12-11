4133074f064e0cffe5f7065bdc6fef40d891f1fb
Christian Maddox <christian.maddox93@gmail.com>
2024-12-11 14:57:35 -0500

    Fixed logout URLs. (#1051)

--- test/arrow_web/controllers/auth_controller_test.exs
+++ test/arrow_web/controllers/auth_controller_test.exs
@@ -41,4 +41,38 @@ defmodule ArrowWeb.Controllers.AuthControllerTest do
       assert response =~ "unauthenticated"
     end
   end
+
+  describe "logout" do
+    test "logs user out and redirects to keycloak logout", %{conn: conn} do
+      current_time = System.system_time(:second)
+
+      auth = %Ueberauth.Auth{
+        uid: "foo@mbta.com",
+        provider: :keycloak,
+        credentials: %Ueberauth.Auth.Credentials{
+          expires_at: current_time + 1_000,
+          other: %{id_token: "id_token"}
+        },
+        extra: %{
+          raw_info: %UeberauthOidcc.RawInfo{
+            userinfo: %{
+              "resource_access" => %{
+                "fake_client" => %{"roles" => ["admin"]}
+              }
+            }
+          }
+        }
+      }
+
+      conn =
+        conn
+        |> assign(:ueberauth_auth, auth)
+        |> get(ArrowWeb.Router.Helpers.auth_path(conn, :callback, "keycloak"))
+
+      assert Guardian.Plug.authenticated?(conn)
+      conn = get(conn, ArrowWeb.Router.Helpers.auth_path(conn, :logout, "keycloak"))
+
+      refute Guardian.Plug.authenticated?(conn)
+    end
+  end
 end
