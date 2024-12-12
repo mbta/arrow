9d238f50e1612337850050a697a074418b1a8155
JD <jd@rudiejd.io>
2024-12-12 11:36:38 -0500

    fixup: remove accidental change in copy task

--- lib/mix/tasks/copy_db.ex
+++ lib/mix/tasks/copy_db.ex
@@ -31,6 +31,7 @@ defmodule Mix.Tasks.CopyDb do
              Enum.map(data, fn map ->
                Map.new(map, fn {key, value} ->
                  parsed_value = parse_json_value(value)
+
                  {String.to_existing_atom(key), parsed_value}
                end)
              end)}
