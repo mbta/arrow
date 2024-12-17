427e46af4836f50ce216674d1a8d0131557cc411
JD <jd@rudiejd.io>
2024-12-17 16:37:35 -0500

    fixup: format

--- assets/src/components/stops/StopLayerControl.tsx
+++ assets/src/components/stops/StopLayerControl.tsx
@@ -15,25 +15,29 @@ const shuttleLayerName = generateLegend(
 const busLayerName = generateLegend(existingBusStopColor, "Existing Bus Stops")
 const selectedStopLayerName = generateLegend(newStopColor, "Selected Stop")
 
-const haversineDistance = (pos: [number, number], otherPos: [number, number]) => {
+const haversineDistance = (
+  pos: [number, number],
+  otherPos: [number, number]
+) => {
   // radius in miles
-  const earthRadius = 3963.1;
+  const earthRadius = 3963.1
 
-  const deltaLatitude = (otherPos[0] - pos[0]) * Math.PI / 180;
-  const deltaLongitude = (otherPos[1] - pos[1]) * Math.PI / 180;
+  const deltaLatitude = ((otherPos[0] - pos[0]) * Math.PI) / 180
+  const deltaLongitude = ((otherPos[1] - pos[1]) * Math.PI) / 180
 
   // https://en.wikipedia.org/wiki/Haversine_formula
-  const arc = Math.cos(
-    pos[0] * Math.PI / 180) * Math.cos(otherPos[0] * Math.PI / 180)
-    * Math.sin(deltaLongitude / 2) * Math.sin(deltaLongitude / 2)
-    + Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2);
-  const line = 2 * Math.atan2(Math.sqrt(arc), Math.sqrt(1 - arc));
+  const arc =
+    Math.cos((pos[0] * Math.PI) / 180) *
+      Math.cos((otherPos[0] * Math.PI) / 180) *
+      Math.sin(deltaLongitude / 2) *
+      Math.sin(deltaLongitude / 2) +
+    Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2)
+  const line = 2 * Math.atan2(Math.sqrt(arc), Math.sqrt(1 - arc))
 
-  return earthRadius * line;
+  return earthRadius * line
 }
 
-const MAX_RADIUS_FOR_STOPS_MILES = 1;
-
+const MAX_RADIUS_FOR_STOPS_MILES = 1
 
 const StopLayerControl = ({
   selectedStop,
@@ -54,17 +58,24 @@ const StopLayerControl = ({
         <LayersControl.Overlay name={shuttleLayerName}>
           <LayerGroup>
             {existingShuttleStops
-              .filter(shuttleStop => haversineDistance([shuttleStop.stop_lat, shuttleStop.stop_lon], [selectedStop.stop_lat, selectedStop.stop_lon]) <= MAX_RADIUS_FOR_STOPS_MILES)
+              .filter(
+                (shuttleStop) =>
+                  haversineDistance(
+                    [shuttleStop.stop_lat, shuttleStop.stop_lon],
+                    [selectedStop.stop_lat, selectedStop.stop_lon]
+                  ) <= MAX_RADIUS_FOR_STOPS_MILES
+              )
               .map(
                 (s, idx) =>
                   s.stop_lat &&
-                  s.stop_lon && (<Marker
-                    key={`${idx}-${s.stop_name}-shuttle-marker`}
-                    position={[s.stop_lat, s.stop_lon]}
-                    icon={genIcon(existingShuttleStopColor)}
-                  >
-                    <Popup>{s.stop_name}</Popup>
-                  </Marker>
+                  s.stop_lon && (
+                    <Marker
+                      key={`${idx}-${s.stop_name}-shuttle-marker`}
+                      position={[s.stop_lat, s.stop_lon]}
+                      icon={genIcon(existingShuttleStopColor)}
+                    >
+                      <Popup>{s.stop_name}</Popup>
+                    </Marker>
                   )
               )}
           </LayerGroup>
@@ -74,32 +85,37 @@ const StopLayerControl = ({
         <LayersControl.Overlay name={busLayerName}>
           <LayerGroup>
             {existingBusStops
-              .filter(gtfsStop => haversineDistance([gtfsStop.lat, gtfsStop.lon], [selectedStop.stop_lat, selectedStop.stop_lon]) <= MAX_RADIUS_FOR_STOPS_MILES)
+              .filter(
+                (gtfsStop) =>
+                  haversineDistance(
+                    [gtfsStop.lat, gtfsStop.lon],
+                    [selectedStop.stop_lat, selectedStop.stop_lon]
+                  ) <= MAX_RADIUS_FOR_STOPS_MILES
+              )
               .map(
-              (s, idx) =>
-                s.lat &&
-                s.lon && (
-                  <Marker
-                    key={`${idx}-${s.name}-bus-stop-marker`}
-                    position={[s.lat, s.lon]}
-                    icon={genIcon(existingBusStopColor)}
-                  >
-                    <Popup>{s.name}</Popup>
-                  </Marker>
-                )
-            )}
+                (s, idx) =>
+                  s.lat &&
+                  s.lon && (
+                    <Marker
+                      key={`${idx}-${s.name}-bus-stop-marker`}
+                      position={[s.lat, s.lon]}
+                      icon={genIcon(existingBusStopColor)}
+                    >
+                      <Popup>{s.name}</Popup>
+                    </Marker>
+                  )
+              )}
           </LayerGroup>
         </LayersControl.Overlay>
       )}
-
-        <LayersControl.Overlay name={selectedStopLayerName} checked={true}>
-          <Marker
-            position={[selectedStop.stop_lat, selectedStop.stop_lon]}
-            icon={genIcon(newStopColor)}
-          >
-            <Popup>{selectedStop.stop_name}</Popup>
-          </Marker>
-        </LayersControl.Overlay>
+      <LayersControl.Overlay name={selectedStopLayerName} checked={true}>
+        <Marker
+          position={[selectedStop.stop_lat, selectedStop.stop_lon]}
+          icon={genIcon(newStopColor)}
+        >
+          <Popup>{selectedStop.stop_name}</Popup>
+        </Marker>
+      </LayersControl.Overlay>
       )
     </LayersControl>
   )
