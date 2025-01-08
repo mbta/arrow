2eb25a0e65fb73cb975cec7d22a7ece2642da54a
JD <jd@rudiejd.io>
2025-01-08 16:09:23 -0500

    fixup: remove all unused code

--- lib/arrow_web/live/flash.ex
+++
@@ -1,9 +0,0 @@
-defmodule ArrowWeb.Flash do
-  @moduledoc """
-  Methods that allow live components to send "flash" messages to their parent live views
-  """
-  def put_flash!(socket, type, message) do
-    send(self(), {:put_flash, type, message})
-    socket
-  end
-end
