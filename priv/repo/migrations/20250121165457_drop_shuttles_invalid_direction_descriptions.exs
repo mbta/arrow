6441a4cdb503cc2c98b5ffca7d7b7b290832cf33
JD <jd@rudiejd.io>
2025-01-21 14:05:13 -0500

    fixup: fix migrations

--- priv/repo/migrations/20250121165457_drop_shuttles_invalid_direction_descriptions.exs
+++ priv/repo/migrations/20250121165457_drop_shuttles_invalid_direction_descriptions.exs
@@ -1,20 +1,20 @@
 defmodule Arrow.Repo.Migrations.DropShuttlesInvalidDirectionDescriptions do
   use Ecto.Migration
 
-  alias Arrow.Shuttles.Route
-
   defp get_valid_direction_sql_strings() do
     Arrow.Shuttles.Route.direction_desc_values() |> Enum.map(&"'#{&1}'") |> Enum.join(",")
   end
 
   def change do
     execute("""
-    DELETE FROM shuttle_route_stops 
-    WHERE shuttle_route_id NOT IN 
-      (SELECT shuttle_route_id FROM shuttle_routes WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()}));
+      DELETE FROM shuttle_route_stops 
+      WHERE shuttle_route_id NOT IN 
+        (SELECT shuttle_route_id FROM shuttle_routes WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()}));
+    """)
 
-    DELETE FROM shuttle_routes 
-    WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()});
+    execute("""
+      DELETE FROM shuttle_routes 
+      WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()});
     """)
   end
 end
