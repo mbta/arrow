ec96dcf11dc8ad43526b4c3523df8d86b560f346
JD <jd@rudiejd.io>
2025-01-21 15:01:02 -0500

    fixup: add down argument to execute

--- priv/repo/migrations/20250121165457_drop_shuttles_invalid_direction_descriptions.exs
+++ priv/repo/migrations/20250121165457_drop_shuttles_invalid_direction_descriptions.exs
@@ -5,16 +5,22 @@ defmodule Arrow.Repo.Migrations.DropShuttlesInvalidDirectionDescriptions do
     Arrow.Shuttles.Route.direction_desc_values() |> Enum.map(&"'#{&1}'") |> Enum.join(",")
   end
 
-  def change do
-    execute("""
-      DELETE FROM shuttle_route_stops 
-      WHERE shuttle_route_id IN 
-        (SELECT shuttle_route_id FROM shuttle_routes WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()}));
-    """)
+  def up do
+    execute(
+      """
+        DELETE FROM shuttle_route_stops 
+        WHERE shuttle_route_id IN 
+          (SELECT shuttle_route_id FROM shuttle_routes WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()}));
+      """,
+      ""
+    )
 
-    execute("""
-      DELETE FROM shuttle_routes 
-      WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()});
-    """)
+    execute(
+      """
+        DELETE FROM shuttle_routes 
+        WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()});
+      """,
+      ""
+    )
   end
 end
