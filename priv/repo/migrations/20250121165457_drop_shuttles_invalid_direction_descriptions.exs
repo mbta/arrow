2d3ae76b557f30214a22fe3e031fd1ec46a4fd07
JD <jd@rudiejd.io>
2025-01-21 13:15:55 -0500

    fixup: add migration to remove invalid values

---
+++ priv/repo/migrations/20250121165457_drop_shuttles_invalid_direction_descriptions.exs
@@ -0,0 +1,15 @@
+defmodule Arrow.Repo.Migrations.DropShuttlesInvalidDirectionDescriptions do
+  use Ecto.Migration
+
+  alias Arrow.Shuttles.Route
+
+  defp get_valid_direction_sql_strings() do
+    Arrow.Shuttles.Route.direction_desc_values() |> Enum.map(&"'#{&1}'") |> Enum.join(",")
+  end
+
+  def change do
+    execute("""
+    DELETE FROM shuttle_routes WHERE direction_desc NOT IN (#{get_valid_direction_sql_strings()})
+    """)
+  end
+end
