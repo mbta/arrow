a6f1691615f5ddaeb58431fed6770a0940215b33
JD <jd@rudiejd.io>
2025-02-07 13:36:11 -0500

    feat(api): add replacement service api

--- lib/arrow_web/controllers/api/limit_controller.ex
+++ lib/arrow_web/controllers/api/limit_controller.ex
@@ -2,11 +2,12 @@ defmodule ArrowWeb.API.LimitController do
   use ArrowWeb, :controller
 
   alias Arrow.Disruptions
+  alias ArrowWeb.API.Util
 
   def index(conn, params) do
-    with {:ok, start_date} <- parse_date(params["start_date"]),
-         {:ok, end_date} <- parse_date(params["end_date"]),
-         :ok <- validate_date_order(start_date, end_date) do
+    with {:ok, start_date} <- Util.parse_date(params["start_date"]),
+         {:ok, end_date} <- Util.parse_date(params["end_date"]),
+         :ok <- Util.validate_date_order(start_date, end_date) do
       limits = Disruptions.get_limits_in_date_range(start_date, end_date)
       render(conn, "index.json-api", data: limits)
     else
@@ -19,23 +20,6 @@ defmodule ArrowWeb.API.LimitController do
         conn
         |> put_status(400)
         |> json(%{error: "End date must be after start date"})
-    end
-  end
-
-  defp parse_date(nil), do: {:error, :invalid_date}
-
-  defp parse_date(date_string) do
-    case Date.from_iso8601(date_string) do
-      {:ok, date} -> {:ok, date}
-      {:error, _} -> {:error, :invalid_date}
-    end
-  end
-
-  defp validate_date_order(start_date, end_date) do
-    if Date.compare(end_date, start_date) in [:gt, :eq] do
-      :ok
-    else
-      {:error, :invalid_date_order}
     end
   end
 end
