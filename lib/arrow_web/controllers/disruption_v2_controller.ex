908c7f574ff0dec39d9aa05fee1411d5dd877e64
cmaddox5 <christian.maddox93@gmail.com>
2025-02-14 11:02:42 -0500

    Added a table for displaying v2 disruptions.

--- lib/arrow_web/controllers/disruption_v2_controller.ex
+++ lib/arrow_web/controllers/disruption_v2_controller.ex
@@ -1,6 +1,7 @@
 defmodule ArrowWeb.DisruptionV2Controller do
   use ArrowWeb, :controller
 
+  alias Arrow.Disruptions
   alias ArrowWeb.Plug.Authorize
   alias Plug.Conn
 
@@ -8,6 +9,9 @@ defmodule ArrowWeb.DisruptionV2Controller do
 
   @spec index(Conn.t(), Conn.params()) :: Conn.t()
   def index(conn, _params) do
-    render(conn, "index.html", user: conn.assigns.current_user)
+    render(conn, "index.html",
+      user: conn.assigns.current_user,
+      disruptions: Disruptions.list_disruptionsv2()
+    )
   end
 end
--- lib/arrow_web/controllers/disruption_v2_html.ex
+++ lib/arrow_web/controllers/disruption_v2_html.ex
@@ -1,8 +1,79 @@
 defmodule ArrowWeb.DisruptionV2View do
   use ArrowWeb, :html
 
+  alias Arrow.Disruptions.DisruptionV2
   alias Arrow.Permissions
   alias Phoenix.Controller
 
   embed_templates "disruption_v2_html/*"
+
+  @route_icon_names %{
+    "Blue" => "blue-line",
+    "Green-B" => "green-line-b",
+    "Green-C" => "green-line-c",
+    "Green-D" => "green-line-d",
+    "Green-E" => "green-line-e",
+    "Mattapan" => "mattapan-line",
+    "Orange" => "orange-line",
+    "Red" => "red-line"
+  }
+
+  @spec route_icon_path(Plug.Conn.t(), atom()) :: String.t()
+  defp route_icon_path(conn, route_id) do
+    Routes.static_path(conn, "/images/icon-#{@route_icon_names[route_id]}-small.svg")
+  end
+
+  defp route_icon(conn, route_id, size, opts) when size in ~w(sm lg) do
+    content_tag(:span, "",
+      class: "m-icon m-icon-#{size} #{Keyword.get(opts, :class, "")}",
+      style: "background-image: url(#{route_icon_path(conn, route_id)})"
+    )
+  end
+
+  defp disrupted_routes(%DisruptionV2{limits: limits}) do
+    limits |> Enum.map(& &1.route.id) |> Enum.uniq()
+  end
+
+  defp get_dates(%DisruptionV2{
+         limits: limits,
+         replacement_services: replacement_services
+       })
+       when limits == [] and replacement_services == [] do
+    {nil, nil}
+  end
+
+  defp get_dates(%DisruptionV2{
+         limits: limits,
+         replacement_services: replacement_services
+       }) do
+    min_start_limit = limits |> Enum.map(& &1.start_date) |> find_minumum()
+    max_end_limit = limits |> Enum.map(& &1.end_date) |> find_maximum()
+
+    min_start_replacement_service =
+      replacement_services |> Enum.map(& &1.start_date) |> find_minumum()
+
+    max_end_replacement_service =
+      replacement_services |> Enum.map(& &1.end_date) |> find_maximum()
+
+    {Enum.min([min_start_limit, min_start_replacement_service], Date),
+     Enum.max([max_end_limit, max_end_replacement_service], Date)}
+  end
+
+  defp find_minumum([]), do: ~D[9999-12-31]
+
+  defp find_minumum(dates) do
+    Enum.min(dates, Date)
+  end
+
+  defp find_maximum([]), do: ~D[0000-01-01]
+
+  defp find_maximum(dates) do
+    Enum.max(dates, Date)
+  end
+
+  defp format_date(nil), do: "N/A"
+
+  defp format_date(date) do
+    Calendar.strftime(date, "%m/%d/%y")
+  end
 end
