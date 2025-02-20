ddfac1ca4342ad009be06d2b36c6f681b64b28c3
Christian Maddox <christian.maddox93@gmail.com>
2025-02-20 10:01:42 -0500

    feat: Disruptions V2 Table (#1123)
    * Added a table for displaying v2 disruptions.
    
    * Added integration tests.
    
    * Apply refactor suggestions from code review.

--- lib/arrow_web/controllers/disruption_v2_html.ex
+++ lib/arrow_web/controllers/disruption_v2_html.ex
@@ -1,8 +1,70 @@
 defmodule ArrowWeb.DisruptionV2View do
   use ArrowWeb, :html
 
+  alias Arrow.Disruptions.DisruptionV2
   alias Arrow.Permissions
   alias Phoenix.Controller
 
   embed_templates "disruption_v2_html/*"
+
+  @route_icon_names %{
+    "Blue" => "blue-line",
+    "Green-B" => "green-line-b",
+    "Green-C" => "green-line-c",
+    "Green-D" => "green-line-d",
+    "Green-E" => "green-line-e",
+    "Mattapan" => "mattapan-line",
+    "Orange" => "orange-line",
+    "Red" => "red-line"
+  }
+
+  attr :conn, Plug.Conn, required: true
+  attr :route_id, :string, required: true
+  attr :size, :string, values: ~w(sm lg), required: true
+  attr :class, :string, default: ""
+
+  defp route_icon(assigns) do
+    ~H"""
+    <span
+      class={"m-icon m-icon-#{@size} #{@class}"}
+      style={"background-image: url(#{route_icon_path(@conn, @route_id)})"}
+    />
+    """
+  end
+
+  @spec route_icon_path(Plug.Conn.t(), atom()) :: String.t()
+  defp route_icon_path(conn, route_id) do
+    Routes.static_path(conn, "/images/icon-#{@route_icon_names[route_id]}-small.svg")
+  end
+
+  defp disrupted_routes(%DisruptionV2{limits: limits}) do
+    limits |> Enum.map(& &1.route.id) |> Enum.uniq()
+  end
+
+  defp get_dates(%DisruptionV2{limits: [], replacement_services: []}) do
+    {nil, nil}
+  end
+
+  defp get_dates(%DisruptionV2{
+         limits: limits,
+         replacement_services: replacement_services
+       }) do
+    min_date =
+      (limits ++ replacement_services)
+      |> Enum.map(& &1.start_date)
+      |> Enum.min(Date, fn -> ~D[9999-12-31] end)
+
+    max_date =
+      (limits ++ replacement_services)
+      |> Enum.map(& &1.end_date)
+      |> Enum.max(Date, fn -> ~D[0000-01-01] end)
+
+    {min_date, max_date}
+  end
+
+  defp format_date(nil), do: "N/A"
+
+  defp format_date(date) do
+    Calendar.strftime(date, "%m/%d/%y")
+  end
 end
---
+++ test/integration/disruptions_v2_test.exs
@@ -0,0 +1,47 @@
+defmodule Arrow.Integration.DisruptionsV2Test do
+  use ExUnit.Case, async: true
+  use Wallaby.Feature
+  import Wallaby.Browser, except: [text: 1]
+  import Arrow.{DisruptionsFixtures, LimitsFixtures}
+
+  @moduletag :integration
+
+  feature "can view disruption on home page", %{session: session} do
+    disruption =
+      create_disruption(%{start_date: ~D[2024-12-31], end_date: ~D[2025-01-01]}, %{
+        start_date: ~D[2024-01-01],
+        end_date: ~D[2024-12-01]
+      })
+
+    session
+    |> visit("/disruptionsv2")
+    |> assert_text(disruption.title)
+    |> assert_text("01/01/24")
+    |> assert_text("01/01/25")
+  end
+
+  feature "shows N/A dates if no limits or replacement service", %{session: session} do
+    disruption_v2_fixture()
+
+    session
+    |> visit("/disruptionsv2")
+    |> assert_text("N/A")
+    |> assert_text("N/A")
+  end
+
+  defp create_disruption(limit_attrs, replacement_service_attrs) do
+    disruption_v2 = disruption_v2_fixture()
+    limit = limit_fixture(Map.put_new(limit_attrs, :disruption_id, disruption_v2.id))
+    day_of_week = limit_day_of_week_fixture(limit_id: limit.id)
+
+    replacement_service =
+      replacement_service_fixture(
+        Map.put_new(replacement_service_attrs, :disruption_id, disruption_v2.id)
+      )
+
+    struct(disruption_v2,
+      limits: [struct(limit, limit_day_of_weeks: [day_of_week])],
+      replacement_services: [struct(replacement_service)]
+    )
+  end
+end
