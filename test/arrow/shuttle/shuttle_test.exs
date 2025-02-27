5b3bbe3334437527aa8cb0dfcd1ca3250606007e
Eddie Maldonado <emaldonado@mbta.com>
2025-02-27 10:35:33 -0500

    fix: prevent setting shuttle to inactive when in use

--- test/arrow/shuttle/shuttle_test.exs
+++ test/arrow/shuttle/shuttle_test.exs
@@ -121,5 +121,119 @@ defmodule Arrow.Shuttles.ShuttleTest do
 
       assert %Ecto.Changeset{valid?: true} = changeset
     end
+
+    test "cannot mark a shuttle as inactive when in use by a replacement service" do
+      shuttle = shuttle_fixture()
+      [route0, route1] = shuttle.routes
+
+      [stop1, stop2, stop3, stop4] = insert_list(4, :gtfs_stop)
+
+      route0
+      |> Arrow.Shuttles.Route.changeset(%{
+        "route_stops" => [
+          %{
+            "direction_id" => "0",
+            "stop_sequence" => "1",
+            "display_stop_id" => stop1.id,
+            "time_to_next_stop" => 30.0
+          },
+          %{
+            "direction_id" => "0",
+            "stop_sequence" => "2",
+            "display_stop_id" => stop2.id
+          }
+        ]
+      })
+      |> Arrow.Repo.update()
+
+      route1
+      |> Arrow.Shuttles.Route.changeset(%{
+        "route_stops" => [
+          %{
+            "direction_id" => "1",
+            "stop_sequence" => "1",
+            "display_stop_id" => stop3.id,
+            "time_to_next_stop" => 30.0
+          },
+          %{
+            "direction_id" => "0",
+            "stop_sequence" => "2",
+            "display_stop_id" => stop4.id
+          }
+        ]
+      })
+      |> Arrow.Repo.update()
+
+      {:ok, shuttle} =
+        shuttle.id
+        |> Arrow.Shuttles.get_shuttle!()
+        |> Shuttle.changeset(%{status: :active})
+        |> Arrow.Repo.update()
+
+      insert(:replacement_service, shuttle: shuttle)
+
+      changeset = Shuttle.changeset(shuttle, %{status: :draft})
+
+      assert %Ecto.Changeset{
+               valid?: false,
+               errors: [
+                 status:
+                   {"cannot set to a non-active status while in use as a replacement servie", []}
+               ]
+             } = changeset
+    end
+
+    test "can mark a shuttle as inactive when not in use by a replacement service" do
+      shuttle = shuttle_fixture()
+      [route0, route1] = shuttle.routes
+
+      [stop1, stop2, stop3, stop4] = insert_list(4, :gtfs_stop)
+
+      route0
+      |> Arrow.Shuttles.Route.changeset(%{
+        "route_stops" => [
+          %{
+            "direction_id" => "0",
+            "stop_sequence" => "1",
+            "display_stop_id" => stop1.id,
+            "time_to_next_stop" => 30.0
+          },
+          %{
+            "direction_id" => "0",
+            "stop_sequence" => "2",
+            "display_stop_id" => stop2.id
+          }
+        ]
+      })
+      |> Arrow.Repo.update()
+
+      route1
+      |> Arrow.Shuttles.Route.changeset(%{
+        "route_stops" => [
+          %{
+            "direction_id" => "1",
+            "stop_sequence" => "1",
+            "display_stop_id" => stop3.id,
+            "time_to_next_stop" => 30.0
+          },
+          %{
+            "direction_id" => "0",
+            "stop_sequence" => "2",
+            "display_stop_id" => stop4.id
+          }
+        ]
+      })
+      |> Arrow.Repo.update()
+
+      {:ok, shuttle} =
+        shuttle.id
+        |> Arrow.Shuttles.get_shuttle!()
+        |> Shuttle.changeset(%{status: :active})
+        |> Arrow.Repo.update()
+
+      changeset = Shuttle.changeset(shuttle, %{status: :draft})
+
+      assert %Ecto.Changeset{valid?: true} = changeset
+    end
   end
 end
