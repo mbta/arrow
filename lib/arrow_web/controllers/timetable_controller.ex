db9265793377d6461d27278564e9730859d5a087
Walton Hoops <me@waltonhoops.com>
2025-03-18 13:47:56 -0600

    more logging

--- lib/arrow_web/controllers/timetable_controller.ex
+++ lib/arrow_web/controllers/timetable_controller.ex
@@ -15,6 +15,8 @@ defmodule ArrowWeb.TimetableController do
     trips_with_times =
       Disruptions.replacement_service_trips_with_times(replacement_service, day_of_week)
 
+    IO.inspect(trips_with_times, "trips_with_times")
+
     direction_id = Map.get(params, "direction_id", "0")
 
     sample_trip = trips_with_times |> Map.get(direction_id) |> Enum.at(0)
