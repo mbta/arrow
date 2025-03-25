6e5446cda52c779dd3eeb37029eca48ed271643b
Eddie Maldonado <emaldonado@mbta.com>
2025-03-25 09:51:49 -0400

    feat: infer GL branches

--- test/arrow/hastus/export_upload_test.exs
+++ test/arrow/hastus/export_upload_test.exs
@@ -53,5 +53,111 @@ defmodule Arrow.Hastus.ExportUploadTest do
 
       assert {:ok, {:error, "Export does not contain any valid routes"}} = data
     end
+
+    @tag export: "gl_known_variant.zip"
+    test "handles a GL export with a known variant", %{export: export} do
+      line = insert(:gtfs_line, id: "line-Green")
+      route = insert(:gtfs_route, id: "Green-E", line_id: line.id)
+
+      direction = insert(:gtfs_direction, direction_id: 0, route_id: route.id, route: route)
+
+      route_pattern =
+        insert(:gtfs_route_pattern,
+          route_id: route.id,
+          route: route,
+          representative_trip_id: "Test",
+          direction_id: 0
+        )
+
+      insert(:gtfs_stop_time,
+        trip:
+          insert(:gtfs_trip,
+            id: "Test",
+            service: insert(:gtfs_service, id: "canonical"),
+            route: route,
+            route_pattern_id: route_pattern.id,
+            directions: [direction]
+          ),
+        stop: insert(:gtfs_stop, id: "70202")
+      )
+
+      data = ExportUpload.extract_data_from_upload(%{path: "#{@export_dir}/#{export}"}, "uid")
+
+      assert {:ok,
+              {:ok,
+               [
+                 %{name: "LRV12025-hlb15016-Saturday-01"}
+               ], "line-Green", _}} = data
+    end
+
+    @tag export: "gl_unambiguous_branch.zip"
+    test "handles a GL export with unknown variant but unambiguous branch", %{export: export} do
+      line = insert(:gtfs_line, id: "line-Green")
+      route = insert(:gtfs_route, id: "Green-E", line_id: line.id)
+
+      direction = insert(:gtfs_direction, direction_id: 0, route_id: route.id, route: route)
+
+      route_pattern =
+        insert(:gtfs_route_pattern,
+          route_id: route.id,
+          route: route,
+          representative_trip_id: "Test",
+          direction_id: 0
+        )
+
+      insert(:gtfs_stop_time,
+        trip:
+          insert(:gtfs_trip,
+            id: "Test",
+            service: insert(:gtfs_service, id: "canonical"),
+            route: route,
+            route_pattern_id: route_pattern.id,
+            directions: [direction]
+          ),
+        stop: insert(:gtfs_stop, id: "70202")
+      )
+
+      data = ExportUpload.extract_data_from_upload(%{path: "#{@export_dir}/#{export}"}, "uid")
+
+      assert {:ok,
+              {:ok,
+               [
+                 %{name: "LRV12025-hlb15016-Saturday-01"}
+               ], "line-Green", _}} = data
+    end
+
+    @tag export: "gl_trips_ambiguous_branch.zip"
+    test "gives validation errors for GL export with ambiguous branches", %{export: export} do
+      line = insert(:gtfs_line, id: "line-Green")
+      route = insert(:gtfs_route, id: "Green-E", line_id: line.id)
+
+      direction = insert(:gtfs_direction, direction_id: 0, route_id: route.id, route: route)
+
+      route_pattern =
+        insert(:gtfs_route_pattern,
+          route_id: route.id,
+          route: route,
+          representative_trip_id: "Test",
+          direction_id: 0
+        )
+
+      insert(:gtfs_stop_time,
+        trip:
+          insert(:gtfs_trip,
+            id: "Test",
+            route: route,
+            route_pattern_id: route_pattern.id,
+            directions: [direction]
+          ),
+        stop: insert(:gtfs_stop, id: "70504")
+      )
+
+      data = ExportUpload.extract_data_from_upload(%{path: "#{@export_dir}/#{export}"}, "uid")
+
+      assert {:ok,
+              {:error,
+               "Unable to infer the Green Line branch for 800-1428, West, U, 800. Please request the via_variant be updated to the branch name and provide an updated export"}} =
+               data
+    end
   end
 end
