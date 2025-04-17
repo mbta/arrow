b012549c990be34a851051ee42196c71cc7a99a8
Walton Hoops <me@waltonhoops.com>
2025-04-17 13:48:23 -0400

    Update lib/arrow/disruptions/replacement_service.ex
    Co-authored-by: Christian Maddox <christian.maddox93@gmail.com>

--- lib/arrow/disruptions/replacement_service.ex
+++ lib/arrow/disruptions/replacement_service.ex
@@ -83,7 +83,7 @@ defmodule Arrow.Disruptions.ReplacementService do
       is_nil(start_date) or is_nil(end_date) ->
         changeset
 
-      Date.compare(start_date, end_date) not in [:lt, :eq] ->
+      Date.compare(start_date, end_date) == :gt ->
         add_error(changeset, :start_date, "start date should not be after end date")
 
       true ->
