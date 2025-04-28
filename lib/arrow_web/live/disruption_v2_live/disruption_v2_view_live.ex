441edc42b229c725813aace1c036c86cc4b135a3
Walton Hoops <me@waltonhoops.com>
2025-04-28 14:38:25 -0600

    cleanup: remove orphaned code

--- lib/arrow_web/live/disruption_v2_live/disruption_v2_view_live.ex
+++ lib/arrow_web/live/disruption_v2_live/disruption_v2_view_live.ex
@@ -5,7 +5,6 @@ defmodule ArrowWeb.DisruptionV2ViewLive do
   alias Arrow.Disruptions.{DisruptionV2, Limit, ReplacementService}
   alias Arrow.Hastus
   alias Arrow.Hastus.Export
-  alias Arrow.Limits.LimitDayOfWeek
   alias ArrowWeb.DisruptionComponents
 
   @impl true
@@ -57,34 +56,6 @@ defmodule ArrowWeb.DisruptionV2ViewLive do
       icon_paths={@icon_paths}
       editing={@editing}
     />
-
-    <%!-- <.live_component
-        id="limit_section"
-        module={ArrowWeb.LimitSection}
-        limit={@limit_in_form}
-        icon_paths={@icon_paths}
-        disruption={@disruption_v2}
-        disabled?={not is_nil(@hastus_export_in_form) or not is_nil(@replacement_service_in_form)}
-      />
-
-      <.live_component
-        id="hastus_export_section"
-        module={ArrowWeb.HastusExportSection}
-        icon_paths={@icon_paths}
-        hastus_export={@hastus_export_in_form}
-        disruption={@disruption_v2}
-        user_id={@user_id}
-        disabled?={not is_nil(@limit_in_form) or not is_nil(@replacement_service_in_form)}
-      />
-
-      <.live_component
-        id="replacement_service_section"
-        module={ArrowWeb.ReplacementServiceSection}
-        replacement_service={@replacement_service_in_form}
-        disruption={@disruption_v2}
-        icon_paths={@icon_paths}
-        disabled?={not is_nil(@hastus_export_in_form) or not is_nil(@limit_in_form)}
-      /> --%>
     """
   end
 
@@ -96,225 +67,12 @@ defmodule ArrowWeb.DisruptionV2ViewLive do
      |> assign(:user_id, session["current_user"].id)}
   end
 
-  # @impl true
-  # def mount(_params, session, socket) do
-  #   disruption = DisruptionV2.new()
-
-  #   form =
-  #     disruption
-  #     |> Disruptions.change_disruption_v2(%{is_active: false, mode: :subway})
-  #     |> to_form()
-
-  #   socket =
-  #     socket
-  #     |> assign(:form_action, :create)
-  #     |> assign(:title, "create new disruption")
-  #     |> assign(:form, form)
-  #     |> assign(:errors, %{})
-  #     |> assign(:icon_paths, icon_paths(socket))
-  #     |> assign(:disruption_v2, disruption)
-  #     |> assign(:editing, disruption)
-  #     |> assign(:user_id, session["current_user"].id)
-
-  #   {:ok, socket}
-  # end
-
   @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   @impl true
-  def handle_event(
-        "validate",
-        %{"disruption_v2" => disruption_v2_params},
-        %Phoenix.LiveView.Socket{} = socket
-      ) do
-    form =
-      socket.assigns.disruption_v2
-      |> Disruptions.change_disruption_v2(disruption_v2_params)
-      |> to_form(action: :validate)
-
-    {:noreply, assign(socket, form: form)}
-  end
-
-  def handle_event("create", %{"disruption_v2" => disruption_v2_params}, socket) do
-    case Disruptions.create_disruption_v2(disruption_v2_params) do
-      {:ok, disruption} ->
-        {:noreply,
-         socket
-         |> clear_flash()
-         |> assign(:page_title, "Edit Disruption v2")
-         |> assign(:disruption_v2, disruption)
-         |> assign(:form_action, :edit)
-         |> assign(:title, "edit disruption")
-         |> push_patch(to: ~p"/disruptions/#{disruption.id}/edit")
-         |> put_flash(:info, "Disruption created successfully")}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply,
-         socket
-         |> assign(form: to_form(changeset))
-         |> put_flash(:error, "Error when saving disruption!")}
-    end
-  end
-
-  def handle_event("edit", %{"disruption_v2" => disruption_v2_params}, socket) do
-    case Disruptions.update_disruption_v2(socket.assigns.disruption_v2, disruption_v2_params) do
-      {:ok, disruption} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Disruption updated successfully")
-         |> assign(:disruption_v2, disruption)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply,
-         socket
-         |> assign(form: to_form(changeset))
-         |> put_flash(:error, "Error when saving disruption!")}
-    end
-  end
-
-  def handle_event(
-        "add_limit",
-        _,
-        %{assigns: %{disruption_v2: %DisruptionV2{id: nil}, form: form}} = socket
-      ) do
-    case Disruptions.create_disruption_v2(form.params) do
-      {:ok, disruption} ->
-        {:noreply,
-         socket
-         |> clear_flash()
-         |> assign(:page_title, "Edit Disruption v2")
-         |> assign(:disruption_v2, disruption)
-         |> assign(:form_action, :edit)
-         |> assign(:title, "edit disruption")
-         |> push_patch(to: ~p"/disruptions/#{disruption.id}/edit")
-         |> assign(:limit_in_form, Limit.new())
-         |> assign(:replacement_service_in_form, nil)
-         |> assign(:hastus_export_in_form, nil)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply,
-         socket
-         |> assign(form: to_form(changeset))
-         |> put_flash(
-           :errors,
-           {"Error when saving disruption!",
-            ["Please define the disruption before adding a limit"]}
-         )}
-    end
-  end
-
-  def handle_event("add_limit", _, socket) do
-    socket =
-      socket
-      |> clear_flash()
-      |> assign(:limit_in_form, Limit.new())
-
-    {:noreply, socket}
-  end
-
-  def handle_event("edit_limit", %{"limit" => limit_id}, socket) do
-    {parsed_id, _} = Integer.parse(limit_id)
-    limit = Arrow.Limits.get_limit!(parsed_id)
-    day_of_weeks = Enum.map(limit.limit_day_of_weeks, &LimitDayOfWeek.set_all_day_default/1)
-
-    {:noreply, assign(socket, :limit_in_form, %{limit | limit_day_of_weeks: day_of_weeks})}
-  end
-
-  def handle_event("duplicate_limit", %{"limit" => limit_id}, socket) do
-    {parsed_id, _} = Integer.parse(limit_id)
-
-    duplicated_limit =
-      parsed_id
-      |> Limits.get_limit!()
-      |> Map.from_struct()
-      |> Map.drop([:id, :inserted_at, :updated_at, :__meta__])
-
-    duplicated_day_of_weeks =
-      duplicated_limit
-      |> Map.get(:limit_day_of_weeks)
-      |> Enum.map(fn day_of_week ->
-        day =
-          day_of_week
-          |> Map.from_struct()
-          |> Map.drop([:id, :inserted_at, :updated_at, :limit_id, :__meta__, :limit])
-          |> LimitDayOfWeek.set_all_day_default()
-
-        struct(LimitDayOfWeek, day)
-      end)
-
-    {:noreply,
-     assign(socket,
-       limit_in_form:
-         Limit.new(%{
-           duplicated_limit
-           | limit_day_of_weeks: duplicated_day_of_weeks
-         })
-     )}
-  end
-
-  def handle_event(
-        "add_replacement_service",
-        _,
-        %{assigns: %{disruption_v2: %DisruptionV2{id: nil}, form: form}} = socket
-      ) do
-    case Disruptions.create_disruption_v2(form.params) do
-      {:ok, disruption} ->
-        {:noreply,
-         socket
-         |> clear_flash()
-         |> assign(:page_title, "Edit Disruption v2")
-         |> assign(:disruption_v2, disruption)
-         |> assign(:form_action, :edit)
-         |> assign(:title, "edit disruption")
-         |> push_patch(to: ~p"/disruptions/#{disruption.id}/edit")
-         |> assign(:limit_in_form, nil)
-         |> assign(:replacement_service_in_form, %ReplacementService{})
-         |> assign(:hastus_export_in_form, nil)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply,
-         socket
-         |> assign(form: to_form(changeset))
-         |> put_flash(
-           :errors,
-           {"Error when saving disruption!",
-            ["Please define the disruption before adding replacement service"]}
-         )}
-    end
-  end
-
-  def handle_event("add_replacement_service", _, socket) do
-    socket =
-      socket
-      |> clear_flash()
-      |> assign(:replacement_service_in_form, %ReplacementService{})
-
-    {:noreply, socket}
-  end
-
-  def handle_event(
-        "edit_replacement_service",
-        %{"replacement_service" => replacement_service_id},
-        socket
-      ) do
-    {parsed_id, _} = Integer.parse(replacement_service_id)
-    replacement_service = Disruptions.get_replacement_service!(parsed_id)
-
-    {:noreply, assign(socket, :replacement_service_in_form, replacement_service)}
-  end
-
-  def handle_event("upload_hastus_export", _, socket) do
-    socket =
-      socket
-      |> clear_flash()
-      |> assign(:hastus_export_in_form, %Export{services: []})
-
-    {:noreply, socket}
-  end
-
   def handle_event("delete_limit", %{"limit" => limit_id}, socket) do
     {parsed_id, _} = Integer.parse(limit_id)
     limit = Limits.get_limit!(parsed_id)
@@ -382,38 +140,6 @@ defmodule ArrowWeb.DisruptionV2ViewLive do
       {:error, %Ecto.Changeset{} = _changeset} ->
         {:noreply, put_flash(socket, :error, "Error when deleting replacement service!")}
     end
-  end
-
-  @impl true
-  def handle_info(:update_disruption, socket) do
-    disruption = Disruptions.get_disruption_v2!(socket.assigns.disruption_v2.id)
-    form = disruption |> Disruptions.change_disruption_v2() |> to_form()
-
-    {:noreply,
-     socket
-     |> assign(
-       limit_in_form: nil,
-       replacement_service_in_form: nil,
-       hastus_export_in_form: nil,
-       disruption_v2: disruption,
-       form: form
-     )}
-  end
-
-  def handle_info({:put_flash, kind, message}, socket) do
-    {:noreply, put_flash(socket, kind, message)}
-  end
-
-  def handle_info(:cancel_limit_form, socket) do
-    {:noreply, assign(socket, limit_in_form: nil)}
-  end
-
-  def handle_info(:cancel_replacement_service_form, socket) do
-    {:noreply, assign(socket, replacement_service_in_form: nil)}
-  end
-
-  def handle_info(:cancel_hastus_export_form, socket) do
-    {:noreply, assign(socket, hastus_export_in_form: nil)}
   end
 
   @adjustment_kind_icon_names %{
