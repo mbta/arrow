88513346f2e7732a7a967e25c91338ddb944b6f9
Walton Hoops <me@waltonhoops.com>
2025-06-03 15:00:57 -0600

    refactor: move shape fetching into route validation

--- lib/arrow/shuttles/route.ex
+++ lib/arrow/shuttles/route.ex
@@ -3,7 +3,9 @@ defmodule Arrow.Shuttles.Route do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Arrow.Repo
   alias Arrow.Shuttles
+  alias Arrow.Shuttles.ShapesUpload
 
   @direction_0_desc_values [:Outbound, :South, :West]
   @direction_1_desc_values [:Inbound, :North, :East]
@@ -43,9 +45,18 @@ defmodule Arrow.Shuttles.Route do
   end
 
   @doc false
-  def changeset(route, attrs, coordinates \\ nil) do
-    route
-    |> cast(attrs, [:direction_id, :direction_desc, :destination, :waypoint, :shape_id])
+  def changeset(route, attrs, active? \\ false) do
+    changeset =
+      cast(route, attrs, [:direction_id, :direction_desc, :destination, :waypoint, :shape_id])
+
+    {coordinates, error} =
+      case get_coordinates(changeset, active?) do
+        {:ok, coordinates} -> {coordinates, nil}
+        {:error, error} -> {nil, error}
+      end
+
+    changeset
+    |> then(&if error, do: add_error(&1, :shape_id, error), else: &1)
     |> cast_assoc(:route_stops,
       with: &Arrow.Shuttles.RouteStop.changeset(&1, &2, coordinates),
       sort_param: :route_stops_sort,
@@ -53,5 +64,53 @@ defmodule Arrow.Shuttles.Route do
     )
     |> validate_required([:direction_id, :direction_desc, :destination])
     |> assoc_constraint(:shape)
+  end
+
+  defp get_coordinates(_route, false), do: {:ok, nil}
+
+  defp get_coordinates(changeset, true) do
+    shape = get_route_shape(changeset)
+
+    if shape do
+      case get_shape_coordinates(shape) do
+        {:ok, :disabled} -> {:ok, nil}
+        {:ok, coordinates} -> {:ok, coordinates}
+        {:error, reason} -> {:error, reason}
+      end
+    else
+      {:ok, nil}
+    end
+  end
+
+  defp get_route_shape(route_changeset) do
+    case get_field(route_changeset, :shape) do
+      %Shuttles.Shape{} = shape ->
+        shape
+
+      nil ->
+        case get_field(route_changeset, :shape_id) do
+          nil -> nil
+          shape_id -> Repo.get(Shuttles.Shape, shape_id)
+        end
+    end
+  end
+
+  defp get_shape_coordinates(shape) do
+    case Shuttles.get_shapes_upload(shape) do
+      {:ok, %Ecto.Changeset{} = changeset} ->
+        coordinates =
+          ShapesUpload.shapes_map_view(changeset)
+          |> Map.get(:shapes)
+          |> List.first()
+          |> Map.get(:coordinates)
+
+        {:ok, coordinates}
+
+      {:ok, :disabled} ->
+        {:ok, :disabled}
+
+      error ->
+        error
+    end
   end
 end
