f54712626b00eb4b2ac1b33057450e3e93f48b24
JD <jd@rudiejd.io>
2026-01-09 16:42:29 -0500

    feat(trainsformer): display information about trainsformer exports

--- lib/arrow/trainsformer/export.ex
+++ lib/arrow/trainsformer/export.ex
@@ -5,10 +5,11 @@ defmodule Arrow.Trainsformer.Export do
   import Ecto.Changeset
 
   alias Arrow.Disruptions.DisruptionV2
-  alias Arrow.Hastus.Service
+  alias Arrow.Trainsformer.Service
 
   typed_schema "trainsformer_exports" do
     field :s3_path, :string
+    field :name, :string
 
     has_many :services, Service, on_replace: :delete, foreign_key: :export_id
     belongs_to :disruption, DisruptionV2
@@ -19,8 +20,8 @@ defmodule Arrow.Trainsformer.Export do
   @doc false
   def changeset(export, attrs) do
     export
-    |> cast(attrs, [:s3_path, :disruption_id])
-    |> cast_assoc(:services, with: &Service.changeset/2)
+    |> cast(attrs, [:s3_path, :disruption_id, :name])
+    |> cast_assoc(:services, with: &Service.changeset/2, required: true)
     |> validate_required([:s3_path])
     |> assoc_constraint(:disruption)
   end
