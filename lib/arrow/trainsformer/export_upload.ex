724efdbee6b37f98ee9889af6d5edcc3266a4094
JD <jd@rudiejd.io>
2026-01-14 10:40:37 -0500

    fixup: simplify getting transfers

--- lib/arrow/trainsformer/export_upload.ex
+++ lib/arrow/trainsformer/export_upload.ex
@@ -272,12 +272,7 @@ defmodule Arrow.Trainsformer.ExportUpload do
       end)
       |> MapSet.new(fn {trip_id, _stop_ids} -> trip_id end)
 
-    trips_with_transfers =
-      if transfers != [] do
-        get_trips_with_transfers_from_file(transfers)
-      else
-        MapSet.new()
-      end
+    trips_with_transfers = get_trips_with_transfers_from_file(transfers)
 
     trips_needing_transfers_without_transfers =
       MapSet.difference(trips_needing_transfers, trips_with_transfers)
@@ -290,8 +285,7 @@ defmodule Arrow.Trainsformer.ExportUpload do
   end
 
   defp get_trips_with_transfers_from_file(transfers) do
-    transfers
-    |> Enum.reduce(MapSet.new(), fn transfer, trip_ids ->
+    Enum.reduce(transfers, MapSet.new(), fn transfer, trip_ids ->
       if transfer.transfer_type == "1" and transfer.from_trip_id != "" and
            transfer.to_trip_id != "" do
         trip_ids
