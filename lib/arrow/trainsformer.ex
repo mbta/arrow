396d5196e9deae11ff7187ad6e161053773b5368
Kent Hansen <khansen@mbta.com>
2026-01-16 16:22:10 -0500

    fix: credo

--- lib/arrow/trainsformer.ex
+++ lib/arrow/trainsformer.ex
@@ -331,7 +331,8 @@ defmodule Arrow.Trainsformer do
   def export_download_url(%Export{s3_path: "s3://" <> s3_path}) do
     [bucket, path] = String.split(s3_path, "/", parts: 2)
 
-    ExAws.Config.new(:s3)
+    :s3
+    |> ExAws.Config.new()
     |> ExAws.S3.presigned_url(:get, bucket, path)
   end
 end
