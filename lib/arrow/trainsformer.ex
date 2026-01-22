32a09a326f1b56b15d165b6a558120e2ae414375
JD <jd@rudiejd.io>
2026-01-22 11:54:42 -0500

    fixup: fix unit tests

--- lib/arrow/trainsformer.ex
+++ lib/arrow/trainsformer.ex
@@ -230,7 +230,7 @@ defmodule Arrow.Trainsformer do
 
   """
   def get_service_date!(id),
-    do: id |> Repo.get!(ServiceDate) |> Repo.preload(:service_date_days_of_week)
+    do: id |> then(&Repo.get!(ServiceDate, &1)) |> Repo.preload(:service_date_days_of_week)
 
   @doc """
   Creates a service_date.
