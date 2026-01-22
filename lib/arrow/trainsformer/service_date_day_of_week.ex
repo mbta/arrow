f0bd34ef8fc5cd3a7a7bb2b0e7da313e500cfea8
JD <jd@rudiejd.io>
2026-01-22 12:19:04 -0500

    fixup: dialyzer

--- lib/arrow/trainsformer/service_date_day_of_week.ex
+++ lib/arrow/trainsformer/service_date_day_of_week.ex
@@ -1,10 +1,10 @@
 defmodule Arrow.Trainsformer.ServiceDateDayOfWeek do
   @moduledoc "Describes the days of week for which a service should be active, within some date range"
 
-  use Ecto.Schema
+  use Arrow.Schema
   import Ecto.Changeset
 
-  schema "service_date_days_of_week" do
+  typed_schema "service_date_days_of_week" do
     field :day_name, Ecto.Enum, values: Arrow.Util.DayOfWeek.day_name_values()
     belongs_to :service_date, Arrow.Trainsformer.ServiceDate, on_replace: :delete
 
