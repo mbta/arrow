e6a3e3d958415211c6d6cdfacc03f5cb9fd0dda7
Kent Hansen <khansen@mbta.com>
2026-03-16 11:41:37 -0400

    fix: add route info to export validation

--- lib/arrow_web/components/edit_trainsformer_export_form.ex
+++ lib/arrow_web/components/edit_trainsformer_export_form.ex
@@ -398,12 +398,7 @@ defmodule ArrowWeb.EditTrainsformerExportForm do
 
   @impl true
   def handle_event("validate", %{"export" => export_params}, socket) do
-    uploaded_file_routes = socket.assigns.uploaded_file_routes
-
-    export_params =
-      if is_nil(uploaded_file_routes),
-        do: export_params,
-        else: Map.put(export_params, "routes", uploaded_file_routes)
+    export_params = Map.put(export_params, "routes", socket.assigns.uploaded_file_routes)
 
     form =
       socket.assigns.export
