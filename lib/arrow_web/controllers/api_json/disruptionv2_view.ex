eabec1a23a14a9d139ebf44b8afe6a5e4ccb5cb8
Emily Crandall Fleischman <ecfleischman@mbta.com>
2026-08-04 14:04:20 -0400

    flatten hastus and trainsformer exports

--- lib/arrow_web/controllers/api_json/disruptionv2_view.ex
+++ lib/arrow_web/controllers/api_json/disruptionv2_view.ex
@@ -2,7 +2,23 @@ defmodule ArrowWeb.API.DisruptionV2View do
   use ArrowWeb, :html
   use JaSerializer.PhoenixView
 
-  attributes([:title, :mode, :status, :description])
+  attributes([:title, :mode, :status, :description, :hastus_exports, :trainsformer_exports])
+
+  def hastus_exports(disruption, _conn) do
+    disruption.hastus_exports
+    |> Enum.map(fn export ->
+      {:ok, download_url} = Arrow.Hastus.export_download_url(export)
+      download_url
+    end)
+  end
+
+  def trainsformer_exports(disruption, _conn) do
+    disruption.trainsformer_exports
+    |> Enum.map(fn export ->
+      {:ok, download_url} = Arrow.Trainsformer.export_download_url(export)
+      download_url
+    end)
+  end
 
   has_many :limits,
     serializer: ArrowWeb.API.LimitView,
@@ -10,14 +26,6 @@ defmodule ArrowWeb.API.DisruptionV2View do
 
   has_many :replacement_services,
     serializer: ArrowWeb.API.ReplacementServiceView,
-    include: true
-
-  has_many :hastus_exports,
-    serializer: ArrowWeb.API.HastusExportView,
-    include: true
-
-  has_many :trainsformer_exports,
-    serializer: ArrowWeb.API.TrainsformerExportView,
     include: true
 
   has_many :shuttles,
