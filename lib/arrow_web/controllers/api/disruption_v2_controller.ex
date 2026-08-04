64e78f58eeaab2d985243f41c93cf6293bd3fcfc
Emily Crandall Fleischman <ecfleischman@mbta.com>
2026-08-04 13:46:52 -0400

    include all relevant data

--- lib/arrow_web/controllers/api/disruption_v2_controller.ex
+++ lib/arrow_web/controllers/api/disruption_v2_controller.ex
@@ -11,17 +11,25 @@ defmodule ArrowWeb.API.DisruptionV2Controller do
     data =
       from(d in DisruptionV2,
         where: d.id == ^id,
-        left_join: l in assoc(d, :limits),
-        left_join: rs in assoc(d, :replacement_services),
-        left_join: he in assoc(d, :hastus_exports),
-        left_join: te in assoc(d, :trainsformer_exports),
-        left_join: sh in assoc(rs, :shuttle),
+        left_join: limit in assoc(d, :limits),
+        left_join: replacement_service in assoc(d, :replacement_services),
+        left_join: hastus in assoc(d, :hastus_exports),
+        left_join: trainsformer in assoc(d, :trainsformer_exports),
+        left_join: shuttle in assoc(replacement_service, :shuttle),
+        left_join: route in assoc(shuttle, :routes),
+        left_join: route_stops in assoc(route, :route_stops),
+        left_join: shape in assoc(route, :shape),
+        left_join: gtfs_stop in assoc(route_stops, :gtfs_stop),
+        left_join: stop in assoc(route_stops, :stop),
         preload: [
-          limits: l,
-          replacement_services: {rs, [shuttle: sh]},
-          hastus_exports: he,
-          trainsformer_exports: te,
-          shuttles: sh
+          limits: limit,
+          replacement_services: {replacement_service, [shuttle: shuttle]},
+          hastus_exports: hastus,
+          trainsformer_exports: trainsformer,
+          shuttles: {
+            shuttle,
+            [routes: {route, route_stops: {route_stops, [:gtfs_stop, :stop]}, shape: shape}]
+          }
         ]
       )
       |> Repo.one!()
