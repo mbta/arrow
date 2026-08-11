9592d3299529a51e8bc25ae75b12a3dc907b2743
Emily Crandall Fleischman <ecfleischman@mbta.com>
2026-08-11 15:57:39 -0400

    include shape URI

--- lib/arrow_web/controllers/api_json/shuttle_route_view.ex
+++ lib/arrow_web/controllers/api_json/shuttle_route_view.ex
@@ -2,13 +2,13 @@ defmodule ArrowWeb.API.ShuttleRouteView do
   use ArrowWeb, :html
   use JaSerializer.PhoenixView
 
-  attributes([:destination, :direction_id, :direction_desc, :waypoint, :shape_id])
+  attributes([:destination, :direction_id, :direction_desc, :waypoint, :shape_id, :shape_uri])
 
   has_many :route_stops,
     serializer: ArrowWeb.API.ShuttleRouteStopView,
     include: true
 
-  def shape_id(route, _conn) do
-    route.shape.path |> String.split("/") |> List.last("") |> String.replace(".kml", "")
-  end
+  def shape_id(route, _conn), do: route.shape.name
+
+  def shape_uri(route, _conn), do: "s3://#{route.shape.bucket}/#{route.shape.path}"
 end
