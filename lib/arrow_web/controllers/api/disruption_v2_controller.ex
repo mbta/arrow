4789c168ce4ea1d367220a091c4bc6f2e45b5255
Emily Crandall Fleischman <ecfleischman@mbta.com>
2026-08-18 09:27:06 -0400

    Export a single file for each disruption (#1419)
    * new disruption api
    
    * use json-api for new route
    
    * include all relevant data
    
    * flatten hastus and trainsformer exports
    
    * credo lint
    
    * add link to download disruption json
    
    * add timetables to replacement services
    
    * indirect shuttle relationship
    
    * move mapping logic into controller
    
    * return s3 urls directly
    
    * new tests (incomplete)
    
    * simplify lookup in api controller
    
    I think the joins were just making things more difficult to read, and I
    don't think there's a benefit to doing them in a single query rather
    than in separate queries in the preload.
    
    * fill out test
    
    * style comments
    
    * mention GTFS Creator in download link
    
    * include shape URI
    
    * fix tests
    
    * Use plain JSON instead of JSON API
    
    * update test
    
    * lints
    
    * remove shuttle from returned data
    
    * populate `stop` relation
    
    * better timetable testing
    
    * remove shape preload
    
    * simplify trainsformer service dates output
    
    * validate services structure in test
    
    * add `id` fields
    
    * include shuttle_id in replacement_services

---
+++ lib/arrow_web/controllers/api/disruption_v2_controller.ex
@@ -0,0 +1,127 @@
+defmodule ArrowWeb.API.DisruptionV2Controller do
+  use ArrowWeb, :controller
+  import Ecto.Query, only: [from: 2]
+
+  alias Arrow.Disruptions.{DisruptionV2, Limit, ReplacementService}
+  alias Arrow.{Hastus, Trainsformer}
+  alias Plug.Conn
+
+  @spec index(Conn.t(), map()) :: Conn.t()
+  def index(conn, %{"id" => id}) do
+    data =
+      from(d in DisruptionV2,
+        where: d.id == ^id,
+        preload: [
+          hastus_exports: [
+            :trip_route_directions,
+            services: [:service_dates]
+          ],
+          trainsformer_exports: [
+            :routes,
+            services: [service_dates: [:service_date_days_of_week]]
+          ],
+          limits: [:limit_day_of_weeks],
+          replacement_services: [
+            shuttle: [routes: [route_stops: [:stop]]]
+          ]
+        ]
+      )
+      |> Arrow.Repo.one!()
+      |> update_in([Access.key(:replacement_services), Access.all()], &replacement_service_data/1)
+      |> update_in([Access.key(:hastus_exports), Access.all()], &hastus_export_data/1)
+      |> update_in([Access.key(:trainsformer_exports), Access.all()], &trainsformer_export_data/1)
+      |> update_in([Access.key(:limits), Access.all()], &limit_data/1)
+      |> Map.take([
+        :id,
+        :title,
+        :mode,
+        :status,
+        :description,
+        :replacement_services,
+        :hastus_exports,
+        :trainsformer_exports,
+        :limits
+      ])
+
+    json(conn, data)
+  end
+
+  defp replacement_service_data(%ReplacementService{} = replacement_service) do
+    replacement_service
+    |> ReplacementService.add_timetable()
+    |> Map.take([:id, :reason, :start_date, :end_date, :timetable])
+    |> Map.merge(%{
+      shuttle_name: replacement_service.shuttle.shuttle_name,
+      # shuttle_id is currently used as a key in GTFS creator, but in the
+      # future once we've fully migrated over to this API, we should use
+      # shuttle_name as the key
+      shuttle_id: replacement_service.shuttle.id
+    })
+  end
+
+  defp hastus_export_data(%Hastus.Export{} = export) do
+    export
+    |> update_in([Access.key(:services), Access.all()], &hastus_service_data/1)
+    |> update_in(
+      [Access.key(:trip_route_directions), Access.all()],
+      &Map.take(&1, [:hastus_route_id, :via_variant, :avi_code, :route_id])
+    )
+    |> Map.take([:id, :line_id, :services, :trip_route_directions, :s3_path])
+  end
+
+  defp hastus_service_data(%Hastus.Service{} = service) do
+    service
+    |> update_in(
+      [Access.key(:service_dates), Access.all()],
+      &Map.take(&1, [:start_date, :end_date])
+    )
+    |> Map.take([:id, :name, :service_dates])
+  end
+
+  defp trainsformer_export_data(%Trainsformer.Export{} = export) do
+    export
+    |> update_in([Access.key(:services), Access.all()], &trainsformer_service_data/1)
+    |> update_in([Access.key(:routes), Access.all()], & &1.route_id)
+    |> Map.take([:id, :services, :routes, :s3_path])
+  end
+
+  defp trainsformer_service_data(%Trainsformer.Service{} = service) do
+    service
+    |> update_in(
+      [Access.key(:service_dates), Access.all()],
+      fn %Trainsformer.ServiceDate{} = service_dates ->
+        %{
+          start_date: service_dates.start_date,
+          end_date: service_dates.end_date,
+          days_of_week: Enum.map(service_dates.service_date_days_of_week, & &1.day_name)
+        }
+      end
+    )
+    |> Map.take([:id, :name, :service_dates])
+  end
+
+  defp limit_data(%Limit{} = limit) do
+    limit
+    |> Map.take([
+      :id,
+      :route_id,
+      :start_date,
+      :end_date
+    ])
+    |> Map.merge(%{
+      days:
+        limit.limit_day_of_weeks
+        |> Enum.filter(& &1.active?)
+        |> Map.new(fn dow ->
+          {dow.day_name,
+           %{
+             start_time: dow.start_time,
+             end_time: dow.end_time,
+             is_all_day: is_nil(dow.start_time) and is_nil(dow.end_time)
+           }}
+        end),
+      start_stop: limit.start_stop_id,
+      end_stop: limit.end_stop_id
+    })
+  end
+end
---
+++ test/arrow_web/controllers/api/disruption_v2_controller_test.exs
@@ -0,0 +1,99 @@
+defmodule ArrowWeb.API.DisruptionV2ControllerTest do
+  use ArrowWeb.ConnCase
+
+  import Arrow.{LimitsFixtures, ShuttlesFixtures, Factory}
+
+  describe "index/2" do
+    @tag :authenticated
+    test "gives 404 for non-existing disruption", %{conn: conn} do
+      assert_error_sent 404, fn -> get(conn, "/api/disruption/100") end
+    end
+
+    @tag :authenticated
+    test "includes all data", %{conn: conn} do
+      shuttle = shuttle_fixture(%{}, true, true)
+      replacement_service = insert(:replacement_service, %{shuttle: shuttle})
+      disruption = replacement_service.disruption
+      limit_fixture(disruption_id: disruption.id)
+      Arrow.HastusFixtures.export_fixture(disruption_id: disruption.id)
+      Arrow.TrainsformerFixtures.export_fixture(disruption_id: disruption.id)
+
+      shuttle_id = shuttle.id
+      shuttle_name = shuttle.shuttle_name
+
+      res =
+        conn
+        |> get(~p"/api/disruption/#{disruption.id}")
+        |> json_response(200)
+
+      assert %{
+               "id" => _,
+               "hastus_exports" => [
+                 %{
+                   "id" => _,
+                   "line_id" => _,
+                   "s3_path" => _,
+                   "services" => [
+                     %{
+                       "id" => _,
+                       "name" => _,
+                       "service_dates" => [
+                         %{"start_date" => _, "end_date" => _} | _
+                       ]
+                     }
+                     | _
+                   ]
+                 }
+               ],
+               "trainsformer_exports" => [
+                 %{
+                   "id" => _,
+                   "routes" => _,
+                   "s3_path" => _,
+                   "services" => [
+                     %{
+                       "id" => _,
+                       "name" => _,
+                       "service_dates" => [
+                         %{"start_date" => _, "end_date" => _, "days_of_week" => [_ | _]} | _
+                       ]
+                     }
+                     | _
+                   ]
+                 }
+               ],
+               "replacement_services" => [
+                 %{
+                   "id" => _,
+                   "start_date" => _,
+                   "end_date" => _,
+                   "reason" => _,
+                   "shuttle_id" => ^shuttle_id,
+                   "shuttle_name" => ^shuttle_name,
+                   "timetable" => timetables
+                 }
+               ],
+               "limits" => [
+                 %{
+                   "id" => _,
+                   "start_date" => _,
+                   "end_date" => _,
+                   "start_stop" => _,
+                   "end_stop" => _,
+                   "route_id" => _,
+                   "days" => %{}
+                 }
+               ]
+             } = res
+
+      active_timetables =
+        for {_day, %{"0" => [_ | _] = d0_timetable, "1" => [_ | _] = d1_timetable}} <- timetables,
+            trip <- d0_timetable ++ d1_timetable,
+            %{"stop_id" => stop_id, "stop_time" => _} <- trip do
+          assert not is_nil(stop_id)
+        end
+
+      assert [_ | _] = active_timetables
+    end
+  end
+end
